51b8d6449038643edfc1c617c97b69038d7d8141
Katherine Dykes <katherine.dykes@nrel.gov>
2014-01-25 13:07:22 -0700

    updated se csm

--- src/NREL_WISDEM/LCOE/lcoe_se_csm_assembly.py
+++ src/NREL_WISDEM/LCOE/lcoe_se_csm_assembly.py
@@ -13,19 +13,21 @@ from fusedwind.vartrees.varTrees import Turbine
 
 # NREL cost and scaling model sub-assemblies
 from Turbine_CostsSE.Turbine_CostsSE.Turbine_CostsSE import Turbine_CostsSE
-from TurbineSE.turbine import TurbineSE
+#from TurbineSE.turbine import TurbineSE
+from TurbineSE.turbineSEdummy import TurbineSE # Temporary fix till TurbineSE works on windows
 from Plant_CostsSE.Plant_BOS.NREL_CSM_BOS.nrel_csm_bos import bos_csm_assembly
 from Plant_CostsSE.Plant_OM.NREL_CSM_OM.nrel_csm_om  import om_csm_assembly
 from Plant_FinanceSE.NREL_CSM_FIN.nrel_csm_fin import fin_csm_assembly
 from Plant_AEPSE.Basic_AEP.basic_aep import aep_weibull_assembly
 
+
 class lcoe_se_csm_assembly(ExtendedFinancialAnalysis):
 
     # variables
     blade_number = Int(iotype='in', desc='number of rotor blades')
-    machine_rating = Float(5000.0, units = 'kW', iotype='in', desc= 'rated machine power in kW')
-    rotor_diameter = Float(126.0, units = 'm', iotype='in', desc= 'rotor diameter of the machine')
-    max_tip_speed = Float(80.0, units = 'm/s', iotype='in', desc= 'maximum allowable tip speed for the rotor')
+    #machine_rating = Float(5000.0, units = 'kW', iotype='in', desc= 'rated machine power in kW')
+    #rotor_diameter = Float(126.0, units = 'm', iotype='in', desc= 'rotor diameter of the machine')
+    #max_tip_speed = Float(80.0, units = 'm/s', iotype='in', desc= 'maximum allowable tip speed for the rotor')
     hub_height = Float(90.0, units = 'm', iotype='in', desc='hub height of wind turbine above ground / sea level') #TODO: tie-in with turbine model
     sea_depth = Float(0.0, units = 'm', iotype='in', desc = 'sea depth for offshore wind project')
 
@@ -37,28 +39,28 @@ class lcoe_se_csm_assembly(ExtendedFinancialAnalysis):
                 
     def configure(self):
         
-        super(lcoe_csm_assembly,self).configure()
+        super(lcoe_se_csm_assembly,self).configure()
         
-        self.replace('tcc_a', tcc_csm_assembly())
+        self.replace('tcc_a', Turbine_CostsSE())
         self.replace('bos_a', bos_csm_assembly())
         self.replace('opex_a', om_csm_assembly())
-        self.replace('aep_a', aep_csm_assembly())
+        self.replace('aep_a', aep_weibull_assembly())
         self.replace('fin_a', fin_csm_assembly())
         
         self.add('trb', TurbineSE())
         
-        self.driver.workflow.add(['trb'])
+        self.driver.workflow.add('trb')
 
         # connect i/o to component and assembly inputs
         # turbine configuration
         # rotor
         self.connect('blade_number', ['trb.blade_number', 'tcc_a.blade_number'])
-        self.connect('rotor_diameter', ['trb.rotor_diameter', 'tcc_a.rotor_diameter', 'bos_a.rotor_diameter'])
+        #self.connect('trb.rotor_diameter', ['bos_a.rotor_diameter']) # TODO: not used in basic BOS model
         # drivetrain
-        self.connect('machine_rating', ['trb.machine_rating', 'tcc_a.machine_rating', 'bos_a.machine_rating', 'opex_a.machine_rating'])
+        self.connect('trb.machine_rating', ['bos_a.machine_rating','opex_a.machine_rating'])
         self.connect('drivetrain_design', ['trb.drivetrain_design', 'tcc_a.drivetrain_design'])
         # tower
-        self.connect('hub_height', ['trb..hub_height', 'tcc_a.hub_height', 'bos_a.hub_height'])
+        self.connect('hub_height', ['trb.hub_height', 'tcc_a.hub_height', 'bos_a.hub_height'])
         # plant configuration
         # climate
         self.connect('sea_depth', ['bos_a.sea_depth', 'opex_a.sea_depth', 'fin_a.sea_depth'])
@@ -112,222 +114,10 @@ def example():
 
     lcoe = lcoe_se_csm_assembly()
 
-    import os
-
-    from rotorse.precomp import Profile, Orthotropic2DMaterial, CompositeSection  # TODO: can just pass file names and do this initialization inside of rotor
-    from towerse.tower import TowerWithpBEAM
-    from commonse.environment import PowerWind, TowerSoil
-
-    turbine = TurbineSE()
-
-    rotor = turbine.rotor
-    nacelle = turbine.nacelle
-    tower = turbine.tower
-
-    # --- blade grid ---
-    rotor.initial_aero_grid = np.array([0.02222276, 0.06666667, 0.11111057, 0.16666667, 0.23333333, 0.3, 0.36666667, 0.43333333,
-        0.5, 0.56666667, 0.63333333, 0.7, 0.76666667, 0.83333333, 0.88888943, 0.93333333, 0.97777724])
-    rotor.initial_str_grid = np.array([0.0, 0.00492790457512, 0.00652942887106, 0.00813095316699, 0.00983257273154, 0.0114340970275,
-        0.0130356213234, 0.02222276, 0.024446481932, 0.026048006228, 0.06666667, 0.089508406455, 0.11111057,
-        0.146462614229, 0.16666667, 0.195309105255, 0.23333333, 0.276686558545, 0.3, 0.333640766319, 0.36666667,
-        0.400404310407, 0.43333333, 0.5, 0.520818918408, 0.56666667, 0.602196371696, 0.63333333, 0.667358391486,
-        0.683573824984, 0.7, 0.73242031601, 0.76666667, 0.83333333, 0.88888943, 0.93333333, 0.97777724, 1.0])
-    rotor.idx_cylinder_aero = 3
-    rotor.idx_cylinder_str = 14
-    rotor.hubFraction = 0.025
-
-    # --- geometry -----
-    rotor.hubHt = 80.0
-    rotor.precone = 2.5
-    rotor.tilt = -5.0
-    rotor.yaw = 0.0
-    rotor.nBlades = 3
-    rotor.turbine_class = 'I'
-
-    # --- atmosphere ---
-    rotor.rho = 1.225
-    rotor.mu = 1.81206e-5
-    rotor.shearExp = 0.2
-
-    # --- operational conditions ---
-    rotor.control.Vin = 3.0
-    rotor.control.Vout = 25.0
-    rotor.control.ratedPower = 5e6
-    rotor.control.minOmega = 0.0
-    rotor.control.maxOmega = 12.0
-    rotor.control.tsr = 7.55
-    rotor.control.pitch = 0.0
-
-    rotor.pitch_extreme = 0.0
-    rotor.azimuth_extreme = 0.0
-
-
-    # --- airfoil files ---
-    basepath = os.path.join(os.path.expanduser('~'), 'Dropbox', 'NREL', '5MW_files', '5MW_AFFiles')
-
-    # load all airfoils
-    airfoil_types = [0]*8
-    airfoil_types[0] = os.path.join(basepath, 'Cylinder1.dat')
-    airfoil_types[1] = os.path.join(basepath, 'Cylinder2.dat')
-    airfoil_types[2] = os.path.join(basepath, 'DU40_A17.dat')
-    airfoil_types[3] = os.path.join(basepath, 'DU35_A17.dat')
-    airfoil_types[4] = os.path.join(basepath, 'DU30_A17.dat')
-    airfoil_types[5] = os.path.join(basepath, 'DU25_A17.dat')
-    airfoil_types[6] = os.path.join(basepath, 'DU21_A17.dat')
-    airfoil_types[7] = os.path.join(basepath, 'NACA64_A17.dat')
-
-    # place at appropriate radial stations
-    af_idx = [0, 0, 1, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7]
-
-    n = len(af_idx)
-    af = [0]*n
-    for i in range(n):
-        af[i] = airfoil_types[af_idx[i]]
-    rotor.airfoil_files = af
-
-    # --- aero analysis options ---
-    rotor.npts_coarse_power_curve = 20
-    rotor.npts_spline_power_curve = 200
-    rotor.AEP_loss_factor = 1.0
-    rotor.nSector = 4
-    rotor.drivetrainType = 'geared'
-
-    # --- materials and composite layup  ---
-    basepath = os.path.join(os.path.expanduser('~'), 'Dropbox', 'NREL', '5MW_files', '5MW_PrecompFiles')
-
-    materials = Orthotropic2DMaterial.listFromPreCompFile(os.path.join(basepath, 'materials.inp'))
-
-    ncomp = len(rotor.initial_str_grid)
-    upper = [0]*ncomp
-    lower = [0]*ncomp
-    webs = [0]*ncomp
-    profile = [0]*ncomp
-
-    rotor.leLoc = np.array([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.498, 0.497, 0.465, 0.447, 0.43, 0.411, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4])
-    rotor.sector_idx_strain = [2]*ncomp
-    web1 = np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 0.4114, 0.4102, 0.4094, 0.3876, 0.3755, 0.3639, 0.345, 0.3342, 0.3313, 0.3274, 0.323, 0.3206, 0.3172, 0.3138, 0.3104, 0.307, 0.3003, 0.2982, 0.2935, 0.2899, 0.2867, 0.2833, 0.2817, 0.2799, 0.2767, 0.2731, 0.2664, 0.2607, 0.2562, 0.1886, -1.0])
-    web2 = np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 0.5886, 0.5868, 0.5854, 0.5508, 0.5315, 0.5131, 0.4831, 0.4658, 0.4687, 0.4726, 0.477, 0.4794, 0.4828, 0.4862, 0.4896, 0.493, 0.4997, 0.5018, 0.5065, 0.5101, 0.5133, 0.5167, 0.5183, 0.5201, 0.5233, 0.5269, 0.5336, 0.5393, 0.5438, 0.6114, -1.0])
-    web3 = np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0])
-
-
-    for i in range(ncomp):
-
-        webLoc = []
-        if web1[i] != -1:
-            webLoc.append(web1[i])
-        if web2[i] != -1:
-            webLoc.append(web2[i])
-        if web3[i] != -1:
-            webLoc.append(web3[i])
-
-        upper[i], lower[i], webs[i] = CompositeSection.initFromPreCompLayupFile(os.path.join(basepath, 'layup_' + str(i+1) + '.inp'), webLoc, materials)
-        profile[i] = Profile.initFromPreCompFile(os.path.join(basepath, 'shape_' + str(i+1) + '.inp'))
-
-    rotor.materials = materials
-    rotor.upperCS = upper
-    rotor.lowerCS = lower
-    rotor.websCS = webs
-    rotor.profile = profile
-    # --------------------------------------
-
-
-    # --- structural options ---
-    rotor.g = 9.81
-    rotor.nF = 5
-
-    # --- rotor design variables ---
-    rotor.r_aero = np.array([0.02222276, 0.06666667, 0.11111057, 0.2, 0.23333333, 0.3, 0.36666667, 0.43333333, 0.5,
-        0.56666667, 0.63333333, 0.64, 0.7, 0.83333333, 0.88888943, 0.93333333, 0.97777724])
-    rotor.r_max_chord = 0.23577
-    rotor.chord_sub = [3.2612, 4.5709, 3.3178, 1.4621]
-    rotor.theta_sub = [13.2783, 7.46036, 2.89317, -0.0878099]
-    rotor.bladeLength = 63.0
-
-
-
-    # ---- nacelle --------
-
-    nacelle.gear_ratio = 87.965
-    nacelle.drivetrain_design = 1  # TODO: sync with my drivetrainType variable
-    nacelle.crane = True
-    nacelle.bevel = 0
-    nacelle.gear_configuration = 'epp'
-
-
-    # ---- tower ------
-    tower.replace('wind', PowerWind())
-    # tower.replace('wave', LinearWaves())  # no waves
-    tower.replace('soil', TowerSoil())
-    tower.replace('tower', TowerWithpBEAM())
-
-    # geometry
-    towerToShaft = 2.0
-    towerHt = rotor.hubHt - towerToShaft  # TODO: this needs to be part of a component
-    tower.z = towerHt*np.array([0.0, 0.5, 1.0])
-    tower.d = [6.0, 4.935, 3.87]
-    tower.t = [0.027*1.3, 0.023*1.3, 0.019*1.3]
-    tower.n = [10, 10]
-    tower.n_reinforced = 3
-
-    # damage
-    tower.z_DEL = np.array([0.000, 1.327, 3.982, 6.636, 9.291, 11.945, 14.600, 17.255, 19.909, 22.564, 25.218, 27.873, 30.527, 33.182, 35.836, 38.491, 41.145, 43.800, 46.455, 49.109, 51.764, 54.418, 57.073, 59.727, 62.382, 65.036, 67.691, 70.345, 73.000, 75.655, 78.309, 80.964, 83.618, 86.273, 87.600])
-    tower.M_DEL = 1e3*np.array([8.2940E+003, 8.1518E+003, 7.8831E+003, 7.6099E+003, 7.3359E+003, 7.0577E+003, 6.7821E+003, 6.5119E+003, 6.2391E+003, 5.9707E+003, 5.7070E+003, 5.4500E+003, 5.2015E+003, 4.9588E+003, 4.7202E+003, 4.4884E+003, 4.2577E+003, 4.0246E+003, 3.7942E+003, 3.5664E+003, 3.3406E+003, 3.1184E+003, 2.8977E+003, 2.6811E+003, 2.4719E+003, 2.2663E+003, 2.0673E+003, 1.8769E+003, 1.7017E+003, 1.5479E+003, 1.4207E+003, 1.3304E+003, 1.2780E+003, 1.2673E+003, 1.2761E+003])
-
-    # wind
-    tower.wind.Uref = 12.0  # TODO: this needs to be connected to rated speed
-    tower.wind.zref = towerHt  # TODO: also connect this
-    tower.wind.z0 = 0.0
-    tower.wind.shearExp = 0.2  # TODO: also connect this
-
-    # soil
-    tower.soil.rigid = 6*[True]
-
-
-    # common variables
-    turbine.tower_top_diameter = 3.87  # TODO: this is not connected to the tower yet
-
-    turbine.run()
-
-
-    tower.znodes = tower.tower.z  # TODO: make this an output
-
-    # outputs
-    print 'AEP =', rotor.AEP
-    print 'diameter =', rotor.diameter
-    print 'ratedConditions.V =', rotor.ratedConditions.V
-    print 'ratedConditions.Omega =', rotor.ratedConditions.Omega
-    print 'ratedConditions.pitch =', rotor.ratedConditions.pitch
-    print 'ratedConditions.T =', rotor.ratedConditions.T
-    print 'ratedConditions.Q =', rotor.ratedConditions.Q
-    print 'mass_one_blade =', rotor.mass_one_blade
-    print 'mass_all_blades =', rotor.mass_all_blades
-    print 'I_all_blades =', rotor.I_all_blades
-    print 'freq =', rotor.freq
-    print 'tip_deflection =', rotor.tip_deflection
-    print 'root_bending_moment =', rotor.root_bending_moment
-
-    # outputs
-    print 'tower mass =', tower.mass
-    print 'tower f1 =', tower.f1
-    print 'tower f2 =', tower.f2
-    print 'tower top_deflection =', tower.top_deflection
-    print 'tower z_buckling =', tower.z_buckling
-    print 'tower buckling =', tower.buckling
-
-
-    import matplotlib.pyplot as plt
-    plt.plot(rotor.V, rotor.P)
-    plt.figure()
-    plt.plot(rotor.spline.r_str, rotor.strainU)
-    plt.plot(rotor.spline.r_str, rotor.strainL)
-    plt.plot(rotor.spline.r_str, rotor.eps_crit)
-    plt.ylim([-5e-3, 5e-3])
-    plt.figure()
-    plt.plot(tower.stress, tower.znodes)
-    plt.plot(tower.damage, tower.znodes)
-    plt.show()
-
-    lcoe.replace('trb', 'turbine')
+    lcoe.trb.crane = True
+    lcoe.trb.gear_configuration = 'eep'
+    lcoe.trb.gear_ratio = 97.0
+    lcoe.drivetrain_design = 1
 
     lcoe.execute()
     
