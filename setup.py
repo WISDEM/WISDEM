72cf02c55feaad630032574208d55f7829f661ea
gbarter <garrett.barter@nrel.gov>
2019-07-21 07:17:05 -0600

    some install progress

--- setup.py
+++ setup.py
@@ -2,54 +2,61 @@
 
 import os
 import sys
-from numpy.distutils.core import setup
-from numpy.distutils.misc_util import Configuration
+import platform
+from setuptools import setup, find_packages
+from numpy.distutils.core import setup, Extension
 
-if len(sys.argv) == 1:
-    print("\nTo install, run: python setup.py install --user\n\n"
-          "To build, run: python setup.py build_ext --inplace\n\n"
-          "For help on C-compiler options run: python setup.py build --help-compiler\n\n"
-          "For help on Fortran-compiler options run: python setup.py build --help-fcompiler\n\n"
-          "To specify a Fortran compiler to use run: python setup.py install --user --fcompiler=<fcompiler name>\n\n"
-          "For further help run: python setup.py build --help"
-      )
-    sys.exit(-1)
+os.environ['NPY_DISTUTILS_APPEND_FLAGS'] = '1'
 
-def configuration(parent_package='',top_path=None):
-    config = Configuration(None,parent_package,top_path)
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=True)
-    config.add_subpackage('wisdem')
-    return config
+# CXXFLAGS for pBEAM
+if platform.system() == 'Windows':
+    pbeamArgs = ['-std=gnu++11','-fPIC']
+else:
+    pbeamArgs = ['-std=c++11','-fPIC']
 
+# CFLAGS for pyMAP
+if platform.system() == 'Windows': # For Anaconda
+    pymapArgs = ['-O1', '-m64', '-fPIC', '-std=c99','-DCMINPACK_NO_DLL']
+elif sys.platform == 'cygwin':
+    pymapArgs = ['-O1', '-m64', '-fPIC', '-std=c99']
+elif platform.system() == 'Darwin':
+    pymapArgs = ['-O1', '-m64', '-fno-omit-frame-pointer', '-fPIC']#, '-std=c99']
+else:
+    #pymapArgs = ['-O1', '-m64', '-fPIC', '-std=c99', '-D WITH_LAPACK']
+    pymapArgs = ['-O1', '-m64', '-fPIC', '-std=c99']
+
+# All the extensions
+bemExt     = Extension('_bem',
+                       sources=[os.path.join('wisdem','ccblade','bem.f90')],
+                       extra_compile_args=['-O2','-fPIC'])
+pyframeExt = Extension('_pyframe3dd',
+                       sources=[os.path.join('wisdem','pyframe3dd','src','*.c')])
+precompExt = Extension('_precomp',
+                       sources=[os.path.join('wisdem','rotorse','PreCompPy.f90')],
+                       extra_compile_args=['-O2','-fPIC'])
+pbeamExt   = Extension('_pBEAM',
+                       sources=[os.path.join('wisdem','pBeam','src','*.cpp')],
+                       extra_compile_args=pbeamArgs,
+                       include_dirs=[os.path.join('wisdem','pBeam','src','include')])
+pymapExt   = Extension('_libmap', sources=[os.path.join('wisdem','pymap','src','*.c*')],
+                       extra_compile_args=pymapArgs,
+                       include_dirs=[os.path.join('pymap','lapack')])
+
+# Top-level setup
 setup(
-    name             = 'wisdem',
+    name             = 'WISDEM',
     version          = '2.0.0',
+    description      = 'Wind-Plant Integrated System Design & Engineering Model',
+    long_description =  'WISDEM is a Python package for conducting multidisciplinary analysis and optimization of wind turbines and plants',
+    url              = 'https://github.com/WISDEM/WISDEM',
     author           = 'NREL WISDEM Team',
     author_email     = 'systems.engineering@nrel.gov',
-    maintainer       = 'NREL WISDEM Team',
-    maintainer_email = 'systems.engineering@nrel.gov',
-    description      = 'Wind-Plant Integrated System Design & Engineering Model',
-    long_description = 'WISDEM is a Python package for conducting multidisciplinary analysis and optimization of wind turbines and plants',
-    keywords         = ['wind','systems engineering','mdao'],
+    install_requires = ['openmdao>= 2.0','numpy','scipy','pandas','simpy'],
+    package_data     =  {'WISDEM': []},
+    package_dir      = {'': 'wisdem'},
+    packages         = find_packages(exclude=['docs', 'tests', 'ext']),
     license          = 'Apache License, Version 2.0',
-    platforms        = ['Windows','Linux','Solaris','Mac OS-X','Unix'],
-    classifiers      = ['Development Status :: 4 - Beta',
-                        'Environment :: Console',
-                        'Intended Audience :: Science/Research',
-                        'Intended Audience :: Developers',
-                        'Intended Audience :: Education',
-                        'License :: Apache',
-                        'Operating System :: Microsoft :: Windows',
-                        'Operating System :: POSIX :: Linux',
-                        'Operating System :: Unix',
-                        'Operating System :: MacOS',
-                        'Programming Language :: Python',
-                        'Topic :: Scientific/Engineering',
-                        'Topic :: Software Development',
-                        'Topic :: Education'],
-    configuration = configuration,
+    python_requires  = '>= 3.4',
+    ext_modules      = [bemExt, pyframeExt, precompExt, pbeamExt, pymapExt],
+    zip_safe         = False
 )
-    
