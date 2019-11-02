9f1ee3553cd93a43d362979cfb87dfb70a3fbba9
Pietro Bortolotti <pietro.bortolotti@nrel.gov>
2019-11-02 17:56:12 -0600

    materials done, airfoils in progress

--- wisdem/rotorse/load_IEA_yaml.py
+++ wisdem/rotorse/load_IEA_yaml.py
@@ -14,6 +14,14 @@ from openmdao.api import ExplicitComponent, Group, IndepVarComp, Problem
 
 
 class Wind_Turbine(object):
+    # Pure python class to load the input yaml file and break into few sub-dictionaries, namely:
+    #   - wt_init_options: dictionary with all the inputs that will be passed as options to the openmdao components, such as the length of the arrays
+    #   - blade: dictionary representing the entry blade in the yaml file
+    #   - tower: dictionary representing the entry tower in the yaml file
+    #   - nacelle: dictionary representing the entry nacelle in the yaml file
+    #   - materials: dictionary representing the entry materials in the yaml file
+    #   - airfoils: dictionary representing the entry airfoils in the yaml file
+
     def __init__(self):
 
         # Validate input file against JSON schema
@@ -33,11 +41,13 @@ class Wind_Turbine(object):
 
         wt_init_options = self.openmdao_vectors()        
         blade           = self.wt_ref['components']['blade']
+        tower           = {} # self.wt_ref['components']['tower']
+        nacelle         = {} # self.wt_ref['components']['tower']
         materials       = self.wt_ref['materials']
+        airfoils        = self.wt_ref['airfoils']
         
-        
-        
-        return wt_init_options, blade, materials
+
+        return wt_init_options, blade, tower, nacelle, materials, airfoils
     
     def openmdao_vectors(self):
     
@@ -48,10 +58,13 @@ class Wind_Turbine(object):
         wt_init_options['materials']['n_mat'] = len(self.wt_ref['materials'])
         
         # Airfoils
-        wt_init_options['airfoils']                 = {}
-        wt_init_options['airfoils']['n_airfoils']   = len(self.wt_ref['airfoils'])
+        wt_init_options['airfoils']           = {}
+        wt_init_options['airfoils']['n_af']   = len(self.wt_ref['airfoils'])
+        wt_init_options['airfoils']['n_aoa']  = 200
+        wt_init_options['airfoils']['n_Re']   = 1
+        wt_init_options['airfoils']['n_tab']  = 1
+        wt_init_options['airfoils']['n_xy']   = 200
         
-    
         return wt_init_options
 
     def load_ontology(self, fname_input, validate=False, fname_schema=''):
@@ -84,6 +97,8 @@ class Wind_Turbine(object):
         
         
 class Materials(ExplicitComponent):
+    # Openmdao component with the wind turbine materials coming from the input yaml file. The inputs and outputs are arrays where each entry represents a material
+    
     def initialize(self):
         self.options.declare('mat_init_options')
     
@@ -94,7 +109,7 @@ class Materials(ExplicitComponent):
         
         self.add_discrete_input('name', val=n_mat * [''],                         desc='Array of names of materials.')
         self.add_discrete_input('orth', val=np.zeros(n_mat),                      desc='Array of flags to set whether a material is isotropic (0) or orthtropic (1). Each entry represents a material.')
-        self.add_discrete_input('component', val=np.zeros(n_mat),                 desc='Array of flags to set whether a material is used in a blade: 0 - coating, 1 - sandwich filler , 2 - shell skin, 3 - shear webs, 4 - spar caps, 5 - TE reinf.isotropic.')
+        self.add_discrete_input('component_id', val=np.zeros(n_mat),              desc='Array of flags to set whether a material is used in a blade: 0 - coating, 1 - sandwich filler , 2 - shell skin, 3 - shear webs, 4 - spar caps, 5 - TE reinf.isotropic.')
         
         self.add_input('E',             val=np.zeros([n_mat, 3]), units='Pa',     desc='Matrix of the Youngs moduli of the materials. Each row represents a material, the three columns represent E11, E22 and E33.')
         self.add_input('G',             val=np.zeros([n_mat, 3]), units='Pa',     desc='Matrix of the shear moduli of the materials. Each row represents a material, the three columns represent G12, G13 and G23.')
@@ -119,80 +134,90 @@ class Materials(ExplicitComponent):
         if density_resin==0.:
             exit('Error: a material named resin must be defined in the input yaml')
         
-        fvf = np.zeros(self.n_mat)
-        fwf = np.zeros(self.n_mat)
+        fvf   = np.zeros(self.n_mat)
+        fwf   = np.zeros(self.n_mat)
+        ply_t = np.zeros(self.n_mat)
         
         for i in range(self.n_mat):
-            if discrete_inputs['component'][i] > 1: # It's a composite
+            if discrete_inputs['component_id'][i] > 1: # It's a composite
+                # Formula to estimate the fiber volume fraction fvf from the laminate and the fiber densities
                 fvf[i]  = (inputs['rho'][i] - density_resin) / (inputs['rho_fiber'][i] - density_resin) 
                 if outputs['fvf'][i] > 0.:
                     if abs(fvf[i] - outputs['fvf'][i]) > 1e-3:
                         exit('Error: the fvf of composite ' + discrete_input['name'][i] + ' specified in the yaml is equal to '+ str(outputs['fvf'][i] * 100) + '%, but this value is not compatible to the other values provided. It should instead be equal to ' + str(fvf[i]*100.) + '%')
                 else:
                     outputs['fvf'][i] = fvf[i]
-                
+                # Formula to estimate the fiber weight fraction fwf from the fiber volume fraction and the fiber densities
                 fwf[i]  = inputs['rho_fiber'][i] * outputs['fvf'][i] / (density_resin + ((inputs['rho_fiber'][i] - density_resin) * outputs['fvf'][i]))
                 if outputs['fwf'][i] > 0.:
                     if abs(fwf[i] - outputs['fwf'][i]) > 1e-3:
                         exit('Error: the fwf of composite ' + discrete_input['name'][i] + ' specified in the yaml is equal to '+ str(outputs['fwf'][i] * 100) + '%, but this value is not compatible to the other values provided. It should instead be equal to ' + str(fwf[i]*100.) + '%')
                 else:
                     outputs['fwf'][i] = fwf[i]
-                
-        print(outputs['fwf'])            
-                    
-                    
-        # mat_dictionary[name]['fwf']  = mat_dictionary[name]['fiber_density'] * mat_dictionary[name]['fvf'] / 100. / (density_resin + ((mat_dictionary[name]['fiber_density'] - density_resin) * mat_dictionary[name]['fvf'] / 100.)) * 100.
-        # for i in range(self.n_mat):
-            # if inputs['orth'][i] == 
+                # Formula to estimate the plyt thickness ply_t of a laminate from the aerial density, the laminate density and the fiber weight fraction
+                ply_t[i] = inputs['rho_area_dry'][i] / inputs['rho'][i] / outputs['fwf'][i]
+                if outputs['ply_t'][i] > 0.:
+                    if abs(ply_t[i] - outputs['ply_t'][i]) > 1e-3:
+                        exit('Error: the ply_t of composite ' + discrete_input['name'][i] + ' specified in the yaml is equal to '+ str(outputs['ply_t'][i]) + 'm, but this value is not compatible to the other values provided. It should instead be equal to ' + str(ply_t[i]) + 'm')
+                else:
+                    outputs['ply_t'][i] = ply_t[i]
         
         
         
         
-# class Airfoils(ExplicitComponent):
-    # def initialize(self):
-        # self.options.declare('airfoils_init_options')
+class Airfoils(ExplicitComponent):
+    def initialize(self):
+        self.options.declare('af_init_options')
     
-    # def setup(self):
-        # airfoils_init_options    = self.options['airfoils_init_options']
-        # n_airfoils               = airfoils_init_options['n_airfoils']
+    def setup(self):
+        af_init_options = self.options['af_init_options']
+        n_af            = af_init_options['n_af'] # Number of airfoils
+        n_aoa           = af_init_options['n_aoa']# Number of angle of attacks
+        n_Re            = af_init_options['n_Re'] # Number of Reynolds, so far hard set at 1
+        n_tab           = af_init_options['n_tab']# Number of tabulated data. For distributed aerodynamic control this could be > 1
+        n_xy            = af_init_options['n_xy'] # Number of coordinate points to describe the airfoil geometry
         
-        # # Airfoil properties
-        # # self.add_discrete_output('airfoils', val=[], desc='Spanwise coordinates for aerodynamic analysis')
-        # self.add_output('airfoils_cl',  val=np.zeros((NAFgrid, npts, NRe)), desc='lift coefficients, spanwise')
-        # self.add_output('airfoils_cd',  val=np.zeros((NAFgrid, npts, NRe)), desc='drag coefficients, spanwise')
-        # self.add_output('airfoils_cm',  val=np.zeros((NAFgrid, npts, NRe)), desc='moment coefficients, spanwise')
-        # self.add_output('airfoils_aoa', val=np.zeros((NAFgrid)), units='deg', desc='angle of attack grid for polars')
-        # self.add_output('airfoils_Re',  val=np.zeros((NRe)), desc='Reynolds numbers of polars')
+        # Airfoil properties
+        self.add_input('af_aoa', val=np.zeros(n_aoa),        units='deg',   desc='Grid of the angles of attack used to define the polars of the airfoils. All airfoils defined in openmdao share this grid.')
+        self.add_input('af_Re',  val=np.zeros((n_Re)),                      desc='Grid of the Reynolds numbers used to define the polars of the airfoils. All airfoils defined in openmdao share this grid.')
+        self.add_input('af_tab', val=np.zeros((n_tab)),                     desc='Grid of the values of the "tab" entity used to define the polars of the airfoils. All airfoils defined in openmdao share this grid. The tab could for example represent a flap deflection angle.')
+        self.add_input('af_cl',  val=np.zeros((n_af, n_aoa, n_Re, n_tab)),  desc='4 dimensional array with the lift coefficients of the airfoils. Dimension 0 is aling the different airfoils defined in the yaml, dimension 1 is along the angles of attack, dimension 2 is along the Reynolds number, dimension 3 is along the number of tabs, which may describe multiple sets at the same station, for example in presence of a flap.')
+        self.add_input('af_cd',  val=np.zeros((n_af, n_aoa, n_Re, n_tab)),  desc='4 dimensional array with the drag coefficients of the airfoils. Dimension 0 is aling the different airfoils defined in the yaml, dimension 1 is along the angles of attack, dimension 2 is along the Reynolds number, dimension 3 is along the number of tabs, which may describe multiple sets at the same station, for example in presence of a flap.')
+        self.add_input('af_cm',  val=np.zeros((n_af, n_aoa, n_Re, n_tab)),  desc='4 dimensional array with the moment coefficients of the airfoils. Dimension 0 is aling the different airfoils defined in the yaml, dimension 1 is along the angles of attack, dimension 2 is along the Reynolds number, dimension 3 is along the number of tabs, which may describe multiple sets at the same station, for example in presence of a flap.')
         
-        # # Airfoil coordinates
-        # self.add_output('airfoils_coord_x',  val=np.zeros((200, npts)), desc='x airfoil coordinate, spanwise')
-        # self.add_output('airfoils_coord_y',  val=np.zeros((200, npts)), desc='y airfoil coordinate, spanwise')
+        # Airfoil coordinates
+        self.add_input('af_coord_x',  val=np.zeros((n_af, n_xy)),           desc='Array of the x airfoil coordinates of the n_af airfoils.')
+        self.add_input('af_coord_y',  val=np.zeros((n_af, n_xy)),           desc='Array of the y airfoil coordinates of the n_af airfoils.')
         
         
 
 class WT_Data(Group):
+    # Openmdao group with all wind turbine data
+    
     def initialize(self):
         self.options.declare('wt_init_options')
         
     def setup(self):
         wt_init_options = self.options['wt_init_options']
-        self.add_subsystem('materials', Materials(mat_init_options    = wt_init_options['materials']))
-        # self.add_subsystem('airfoils',  Airfoils(airfoil_init_options = wt_init_options['airfoils']))
+        self.add_subsystem('materials', Materials(mat_init_options = wt_init_options['materials']))
+        self.add_subsystem('airfoils',  Airfoils(af_init_options   = wt_init_options['airfoils']))
 
-def yaml2openmdao(wt_opt, wt_init_options, blade, materials):
+def yaml2openmdao(wt_opt, wt_init_options, blade, tower, nacelle, materials, airfoils):
+    # Function to assign values to the openmdao group WT_Data and all its components
     
     wt_opt = assign_material_values(wt_opt, wt_init_options, materials)
-    
+    wt_opt = assign_airfoils_values(wt_opt, wt_init_options, airfoils)
     
     return wt_opt
     
 def assign_material_values(wt_opt, wt_init_options, materials):
-
+    # Function to assign values to the openmdao component Materials
+    
     n_mat = wt_init_options['materials']['n_mat']
     
     name        = n_mat * ['']
     orth        = np.zeros(n_mat)
-    component   = -np.ones(n_mat)
+    component_id= -np.ones(n_mat)
     rho         = np.zeros(n_mat)
     E           = np.zeros([n_mat, 3])
     G           = np.zeros([n_mat, 3])
@@ -207,8 +232,8 @@ def assign_material_values(wt_opt, wt_init_options, materials):
         name[i] =  materials[i]['name']
         orth[i] =  materials[i]['orth']
         rho[i]  =  materials[i]['rho']
-        if 'component' in materials[i]:
-            component[i] = materials[i]['component']
+        if 'component_id' in materials[i]:
+            component_id[i] = materials[i]['component_id']
         
         if orth[i] == 0:
             if 'E' in materials[i]:
@@ -218,7 +243,7 @@ def assign_material_values(wt_opt, wt_init_options, materials):
             if 'G' in materials[i]:
                 G[i,:]  = np.ones(3) * materials[i]['G']
             elif 'nu' in materials[i]:
-                G[i,:]  = np.ones(3) * materials[i]['E']/(2*(1+materials[i]['nu']))
+                G[i,:]  = np.ones(3) * materials[i]['E']/(2*(1+materials[i]['nu'])) # If G is not provided but the material is isotropic and we have E and nu we can just estimate it
                 warning_shear_modulus_isotropic = 'Ontology input warning: No shear modulus, G, provided for material "%s".  Assuming 2G*(1 + nu) = E, which is only valid for isotropic materials.'%mati['name']
                 print(warning_shear_modulus_isotropic)
                 
@@ -243,39 +268,50 @@ def assign_material_values(wt_opt, wt_init_options, materials):
     wt_opt['materials.name']     = name
     wt_opt['materials.orth']     = orth
     wt_opt['materials.rho']      = rho
-    wt_opt['materials.component']= component
+    wt_opt['materials.component_id']= component_id
     wt_opt['materials.E']        = E
     wt_opt['materials.G']        = G
     wt_opt['materials.nu']       = nu
     wt_opt['materials.rho_fiber']      = rho_fiber
-    wt_opt['materials.rho_area_dry']      = rho_area_dry
+    wt_opt['materials.rho_area_dry']   = rho_area_dry
     wt_opt['materials.fvf']      = fvf
     wt_opt['materials.fwf']      = fwf
-        # print(materials[i])
-        
-        # print(materials[i]['E'])
 
     return wt_opt
+
+def assign_airfoils_values(wt_opt, wt_init_options, airfoils):
+    # Function to assign values to the openmdao component Airfoils
     
-  
+    n_af  = wt_init_options['airfoils']['n_af']
+    n_aoa = wt_init_options['airfoils']['n_aoa']
+    n_Re  = wt_init_options['airfoils']['n_Re']
+    n_tab = wt_init_options['airfoils']['n_tab']
+    n_xy  = wt_init_options['airfoils']['n_xy']
+
+     
+    return wt_opt
+    
+    
+    
+    
 if __name__ == "__main__":
 
     ## File management
-    fname_input        = "turbine_inputs/nrel5mw_mod_update.yaml"
+    # fname_input        = "turbine_inputs/nrel5mw_mod_update.yaml"
+    fname_input        = "/mnt/c/Material/Projects/Hitachi_Design/Design/turbine_inputs/aerospan_formatted_v13.yaml"
     fname_output       = "turbine_inputs/testing_twist.yaml"
     
     wt_initial              = Wind_Turbine()
     wt_initial.validate     = False
     wt_initial.fname_schema = "turbine_inputs/IEAontology_schema.yaml"
-    wt_init_options, blade, materials = wt_initial.initialize(fname_input)
+    wt_init_options, blade, tower, nacelle, materials, airfoils = wt_initial.initialize(fname_input)
     
     wt_opt          = Problem()
     wt_opt.model    = WT_Data(wt_init_options = wt_init_options)
     wt_opt.setup()
-    wt_opt = yaml2openmdao(wt_opt, wt_init_options, blade, materials)
+    wt_opt = yaml2openmdao(wt_opt, wt_init_options, blade, tower, nacelle, materials, airfoils)
     wt_opt.run_driver()
     
-    print(wt_opt['materials.name'])
     
 
 
