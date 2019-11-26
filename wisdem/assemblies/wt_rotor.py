d5e41d645b2a8fa1bc00cb58563b91a9c78dae3f
Bortolotti <ptrbortolotti@gmail.com>
2019-11-26 15:47:24 -0700

    twist optimization up and running. next step chord opt

--- wisdem/assemblies/wt_rotor.py
+++ wisdem/assemblies/wt_rotor.py
@@ -1,19 +1,49 @@
 import numpy as np
-from openmdao.api import ExplicitComponent, Group, IndepVarComp, Problem, SqliteRecorder, ScipyOptimizeDriver
+import os
+import matplotlib.pyplot as plt
+from openmdao.api import ExplicitComponent, Group, IndepVarComp, Problem, SqliteRecorder, ScipyOptimizeDriver, CaseReader
 from wisdem.assemblies.load_IEA_yaml import WT_Data, Wind_Turbine, yaml2openmdao
 from wisdem.rotorse.rotor_geometry import TurbineClass
 from wisdem.rotorse.rotor_aeropower import RegulatedPowerCurve, Cp_Ct_Cq_Tables, NoStallConstraint, AEP
 from wisdem.commonse.environment import PowerWind
 from wisdem.commonse.distribution import WeibullWithMeanCDF
 
+class Opt_Data(object):
+    # Pure python class to set the optimization parameters:
+
+    def __init__(self):
+        
+        self.opt_options = {}
+
+        # Save data
+        self.folder_output    = 'it_0/'
+        self.optimization_log = 'log_opt.sql'
+
+        # Twist optimization parameters
+        self.n_opt_twist = 8
+
+    def initialize(self):
+
+        self.opt_options['folder_output']    = self.folder_output
+        self.opt_options['optimization_log'] = self.folder_output + self.optimization_log
+        
+        self.opt_options['twist'] = {}
+        self.opt_options['twist']['n_opt_twist'] = self.n_opt_twist 
+
+        return self.opt_options
+
+
 class ParametrizeBlade(ExplicitComponent):
     # Openmdao component to parameterize distributed quantities for the aerodynamic only analysis of the wind turbine rotor
     def initialize(self):
         self.options.declare('blade_init_options')
+        self.options.declare('opt_options')
+        
     def setup(self):
         blade_init_options = self.options['blade_init_options']
+        opt_options        = self.options['opt_options']
         n_span             = blade_init_options['n_span']
-        self.n_opt_twist   = n_opt_twist        = 8
+        self.n_opt_twist   = n_opt_twist        = opt_options['twist']['n_opt_twist']
 
         self.add_input('s',               val=np.zeros(n_span),                 desc='1D array of the non-dimensional spanwise grid defined along blade axis (0-blade root, 1-blade tip)')
         self.add_input('twist_original',  val=np.zeros(n_span),    units='rad', desc='1D array of the twist values defined along blade span. The twist is the one defined in the yaml.')
@@ -23,18 +53,13 @@ class ParametrizeBlade(ExplicitComponent):
         self.add_output('twist_param',    val=np.zeros(n_span),    units='rad', desc='1D array of the twist values defined along blade span. The twist is the result of the parameterization.')
 
     def compute(self, inputs, outputs):
-        
-        print('new eval')
-        print(inputs['twist_opt_gain'])
 
         twist_opt_gain_nd       = inputs['twist_opt_gain']
         twist_upper             = np.ones(self.n_opt_twist) * 20.  / 180. * np.pi
         twist_lower             = np.ones(self.n_opt_twist) * -20. / 180. * np.pi
         twist_opt_gain_rad      = twist_opt_gain_nd * (twist_upper - twist_lower) + twist_lower
         twist_opt_gain_rad_interp   = np.interp(inputs['s'], inputs['s_opt_twist'], twist_opt_gain_rad)
-        print(twist_opt_gain_rad_interp)
         outputs['twist_param']  = inputs['twist_original'] + twist_opt_gain_rad_interp
-
 
 class RotorAeroPower(Group):
     # Openmdao group for the aerodynamic only analysis of the wind turbine rotor
@@ -46,20 +71,16 @@ class RotorAeroPower(Group):
         self.add_subsystem('powercurve',        RegulatedPowerCurve(wt_init_options   = wt_init_options), promotes = ['control_Vin', 'control_Vout','control_ratedPower','control_minOmega','control_maxOmega','control_maxTS','control_tsr','control_pitch','drivetrainType','drivetrainEff','r','chord', 'theta','Rhub', 'Rtip', 'hub_height','precone', 'tilt','yaw','precurve','precurveTip','presweep','presweepTip', 'airfoils_aoa','airfoils_Re','airfoils_cl','airfoils_cd','airfoils_cm', 'nBlades', 'rho', 'mu'])
         self.add_subsystem('aeroperf_tables',   Cp_Ct_Cq_Tables(wt_init_options   = wt_init_options), promotes = ['control_Vin', 'control_Vout','r','chord', 'theta','Rhub', 'Rtip', 'hub_height','precone', 'tilt','yaw','precurve','precurveTip','presweep','presweepTip', 'airfoils_aoa','airfoils_Re','airfoils_cl','airfoils_cd','airfoils_cm', 'nBlades', 'rho', 'mu'])
         self.add_subsystem('stall_check',       NoStallConstraint(wt_init_options   = wt_init_options), promotes = ['airfoils_aoa','airfoils_cl','airfoils_cd','airfoils_cm'])
-        # self.add_subsystem('wind',              PowerWind(nPoints=1))
         self.add_subsystem('cdf',               WeibullWithMeanCDF(nspline=200))
         self.add_subsystem('aep',               AEP(), promotes=['AEP'])
 
-
+        # Connections to the stall check
         self.connect('powercurve.aoa_cutin','stall_check.aoa_along_span')
-        # self.connect('hub_height', 'wind.zref')
 
-        # connections to cdf
+        # Connections to the Weibull CDF
         self.connect('powercurve.V_spline', 'cdf.x')
         
-        # self.connect('shape_parameter',     'cdf.k')
-
-        # connections to aep
+        # Connections to the aep computation component
         self.connect('cdf.F',               'aep.CDF_V')
         self.connect('powercurve.P_spline', 'aep.P')
 
@@ -68,20 +89,26 @@ class WT_Rotor(Group):
     
     def initialize(self):
         self.options.declare('wt_init_options')
+        self.options.declare('opt_options')
         
     def setup(self):
         wt_init_options = self.options['wt_init_options']
+        opt_options     = self.options['opt_options']
         
         # Optimization parameters initialized as indipendent variable component
         opt_var = IndepVarComp()
-        opt_var.add_output('twist_opt_gain', val = 0.5 * np.ones(8))
+        opt_var.add_output('twist_opt_gain', val = 0.5 * np.ones(opt_options['twist']['n_opt_twist']))
         self.add_subsystem('opt_var',opt_var)
 
         # Analysis components
         self.add_subsystem('wt',        Wind_Turbine(wt_init_options        = wt_init_options), promotes = ['*'])
         self.add_subsystem('wt_class',  TurbineClass())
-        self.add_subsystem('param',     ParametrizeBlade(blade_init_options = wt_init_options['blade']))
+        self.add_subsystem('param',     ParametrizeBlade(blade_init_options = wt_init_options['blade'], opt_options = opt_options))
         self.add_subsystem('ra',        RotorAeroPower(wt_init_options      = wt_init_options))
+        # Post-processing
+        self.add_subsystem('outputs_2_screen',  Outputs_2_Screen())
+        self.add_subsystem('conv_plots',        Convergence_Trends_Opt(opt_options = opt_options))
+
 
         # Connections to wind turbine class
         self.connect('configuration.ws_class' , 'wt_class.turbine_class')
@@ -118,23 +145,84 @@ class WT_Rotor(Group):
         self.connect('blade.outer_shape_bem.s',     'param.s')
         self.connect('blade.outer_shape_bem.twist', 'param.twist_original')
         self.connect('param.twist_param',           'ra.theta')
+        # Connections to outputs
+        self.connect('ra.AEP', 'outputs_2_screen.AEP')
+
+class Convergence_Trends_Opt(ExplicitComponent):
+    def initialize(self):
+        
+        self.options.declare('opt_options')
+        
+    def compute(self, inputs, outputs):
+        
+        folder_output       = self.options['opt_options']['folder_output']
+        optimization_log    = self.options['opt_options']['optimization_log']
+
+        if os.path.exists(optimization_log):
+        
+            cr = CaseReader(optimization_log)
+            cases = cr.list_cases()
+            rec_data = {}
+            iterations = []
+            for i, casei in enumerate(cases):
+                iterations.append(i)
+                it_data = cr.get_case(casei)
+                
+                # parameters = it_data.get_responses()
+                for parameters in [it_data.get_responses(), it_data.get_design_vars()]:
+                    for j, param in enumerate(parameters.keys()):
+                        if i == 0:
+                            rec_data[param] = []
+                        rec_data[param].append(parameters[param])
+
+            for param in rec_data.keys():
+                fig, ax = plt.subplots(1,1,figsize=(5.3, 4))
+                ax.plot(iterations, rec_data[param])
+                ax.set(xlabel='Number of Iterations' , ylabel=param)
+                fig_name = 'Convergence_trend_' + param + '.png'
+                fig.savefig(folder_output + fig_name)
+                plt.close(fig)
+
+# Class to print outputs on screen
+class Outputs_2_Screen(ExplicitComponent):
+    def setup(self):
+        
+        self.add_input('AEP', val=0.0, units = 'GW * h')
+        
+    def compute(self, inputs, outputs):
+        print('########################################')
+        print('Objectives')
+        print('AEP:         {:8.10f} GWh'.format(inputs['AEP'][0]))
+        print('########################################')
 
 if __name__ == "__main__":
 
     ## File management
-    fname_input        = "reference_turbines/nrel5mw/nrel5mw_mod_update.yaml"
+    fname_input    = "reference_turbines/nrel5mw/nrel5mw_mod_update.yaml"
     # fname_input        = "/mnt/c/Material/Projects/Hitachi_Design/Design/turbine_inputs/aerospan_formatted_v13.yaml"
-    fname_output       = "reference_turbines/nrel5mw/nrel5mw_mod_update_output.yaml"
-    
+    fname_output   = "reference_turbines/nrel5mw/nrel5mw_mod_update_output.yaml"
+    folder_output  = 'it_1/'
+    opt_flag       = True
     # Load yaml data into a pure python data structure
     wt_initial               = WT_Data()
     wt_initial.validate      = False
     wt_initial.fname_schema  = "reference_turbines/IEAontology_schema.yaml"
     wt_init_options, wt_init = wt_initial.initialize(fname_input)
     
+    # Optimization options
+    optimization_data       = Opt_Data()
+    optimization_data.folder_output = folder_output
+    if opt_flag = True:
+        optimization_data.n_opt_twist = 8
+    else:
+        optimization_data.n_opt_twist = wt_initial.n_span
+    opt_options             = optimization_data.initialize()
+    if not os.path.isdir(folder_output):
+        os.mkdir(folder_output)
+
     # Initialize openmdao problem
     wt_opt          = Problem()
-    wt_opt.model    = WT_Rotor(wt_init_options = wt_init_options)
+    wt_opt.model    = WT_Rotor(wt_init_options = wt_init_options, opt_options = opt_options)
     wt_opt.model.approx_totals(method='fd')
     
     # Set optimization solver and options
@@ -147,12 +235,12 @@ if __name__ == "__main__":
     wt_opt.model.add_objective('ra.AEP', scaler = -1.e-6)
     
     # Set optimization variables
-    indices_no_root         = range(2,8)
+    indices_no_root         = range(2,opt_options['twist']['n_opt_twist'])
     wt_opt.model.add_design_var('opt_var.twist_opt_gain', indices = indices_no_root, lower=0., upper=1.)    
     
     # Set recorder
-    wt_opt.driver.add_recorder(SqliteRecorder('log_opt.sql'))
-    wt_opt.driver.recording_options['includes'] = ['AEP','total_blade_cost','lcoe','tip_deflection_ratio']
+    wt_opt.driver.add_recorder(SqliteRecorder(opt_options['optimization_log']))
+    wt_opt.driver.recording_options['includes'] = ['ra.AEP']
     wt_opt.driver.recording_options['record_objectives']  = True
     wt_opt.driver.recording_options['record_constraints'] = True
     wt_opt.driver.recording_options['record_desvars']     = True
@@ -170,9 +258,9 @@ if __name__ == "__main__":
     # Save data coming from openmdao to an output yaml file
     wt_initial.write_ontology(wt_opt, fname_output)
 
+    # Printing and plotting results
     print('AEP = ' + str(wt_opt['ra.AEP']*1.e-6) + ' GWh')
     
-    import matplotlib.pyplot as plt
     plt.figure()
     plt.plot(wt_opt['ra.powercurve.V'], wt_opt['ra.powercurve.P']/1e6)
     plt.xlabel('wind speed (m/s)')
@@ -230,6 +318,6 @@ if __name__ == "__main__":
     plt.yticks(fontsize=12)
     plt.grid(color=[0.8,0.8,0.8], linestyle='--')
     plt.subplots_adjust(bottom = 0.15, left = 0.15)
-    # fig_name = 'aoa.png'
-    # faoa.savefig(folder_output + fig_name)
+    fig_name = 'aoa.png'
+    faoa.savefig(folder_output + fig_name)
     plt.show()
