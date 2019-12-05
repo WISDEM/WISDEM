5af3f6f3d898a7b2aba797b4d751a9b9ce0ea7ac
Evan Gaertner <evan.gaertner@gmail.com>
2019-12-05 10:45:42 -0700

    aeroelasticse: added summary stat option to fast_post, avoides mem issues when running parallel batch with multiprocessing and very large .out files

--- wisdem/aeroelasticse/FAST_post.py
+++ wisdem/aeroelasticse/FAST_post.py
@@ -8,3 +8,13 @@ def return_timeseries(fname):
     data, meta = ReadFASToutFormat(fname, 2, Verbose=True)
     return data
 
+def return_stats(fname):
+    data, meta = ReadFASToutFormat(fname, 2, Verbose=True)
+    stats = {}
+    for var in data.keys():
+        stats[var]['mean']   = np.mean(data[var])
+        stats[var]['min']    = min(data[var])
+        stats[var]['max']    = max(data[var])
+        stats[var]['std']    = np.std((data[var]))
+        stats[var]['absmax'] = np.abs(max(data[var]))
+    return stats
