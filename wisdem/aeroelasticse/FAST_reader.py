227593733c707101a9b6161824ddf74e6addfbec
Evan Gaertner <evan.gaertner@gmail.com>
2020-02-18 16:12:59 -0700

    moordyn read/write fix

--- wisdem/aeroelasticse/FAST_reader.py
+++ wisdem/aeroelasticse/FAST_reader.py
@@ -2122,6 +2122,11 @@ class InputReader_OpenFAST(InputReader_Common):
         self.fst_vt['MAP']['Option']   = [str(val) for val in f.readline().strip().split()]
 
     def read_MoorDyn(self):
+
+        moordyn_file = os.path.normpath(os.path.join(self.FAST_directory, self.fst_vt['Fst']['MooringFile']))
+
+        f = open(moordyn_file)
+
         # MoorDyn
         f.readline()
         f.readline()
