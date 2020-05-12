f4f365324611e864cf030c75c0774de1b02b7fa5
Alicia Key <akey7@users.noreply.github.com>
2020-05-12 13:33:13 -0600

    LandBOSSE second integration
    This adds a deliberately failing test to the test suite to ensure that the LandBOSSE tests are being called.

---
+++ wisdem/test/test_landbosse/test_landbosse.py
@@ -0,0 +1,11 @@
+import pytest
+
+# @pytest.fixture
+# def landbosse_manager_runner():
+#     """
+#     This fixture instantiates LandBOSSE with all defaults
+#     """
+
+
+def test_landbosse_canary():
+    assert False
