a4cc73f79fb621580755adf710f32250b1e5fdd2
Alicia Key <akey7@users.noreply.github.com>
2020-05-12 15:50:28 -0600

    LandBOSSE second integration
    Refactor LandBOSSE model execution to a fixture function.

--- wisdem/test/test_landbosse/test_landbosse.py
+++ wisdem/test/test_landbosse/test_landbosse.py
@@ -4,15 +4,25 @@ from wisdem.landbosse.landbosse_omdao.landbosse import LandBOSSE
 import openmdao.api as om
 
 
-def test_landbosse():
+@pytest.fixture
+def landbosse_costs_by_module_type_operation():
+    """
+    Executes LandBOSSE and extracts cost output for the regression
+    test.
+    """
     prob = om.Problem()
     prob.model = LandBOSSE()
     prob.model.options['topLevelFlag'] = True
     prob.setup()
     prob.run_model()
-
     landbosse_costs_by_module_type_operation = prob['landbosse_costs_by_module_type_operation']
+    return landbosse_costs_by_module_type_operation
 
+
+def test_landbosse(landbosse_costs_by_module_type_operation):
+    """
+    This runs the regression test by comparing against the expected validation
+    data.
+    """
     print(landbosse_costs_by_module_type_operation)
-
     assert True
