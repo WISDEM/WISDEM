0c976e66b9fb264794e0fc1962ca7bc461afc22f
Alicia Key <akey7@users.noreply.github.com>
2020-05-12 14:14:44 -0600

    LandBOSSE second integration
    Update authorship credits.

--- wisdem/test/test_landbosse/__init__.py
+++ wisdem/test/test_landbosse/__init__.py
@@ -1,6 +1,6 @@
 """Tests suite for ORBIT."""
 
-__author__ = ["Alicia Key", "Parangat Bhaskar", "Annika Eberle", "Owen Roberts"]
+__author__ = ["Alicia Key", "Parangat Bhaskar", "Annika Eberle", "Owen Roberts", "Garrett Barter"]
 __copyright__ = "Copyright 2020, National Renewable Energy Laboratory"
 __maintainer__ = "Alicia Key"
 __email__ = "alicia.key@nrel.gov"
