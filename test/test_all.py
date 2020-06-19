37aa0f54021749b4acbfb44a411a78c305af70ef
Garrett Barter <garrett.barter@nrel.gov>
2020-06-19 05:56:37 -0600

    simplified test hook script

--- test/test_all.py
+++ test/test_all.py
@@ -1,15 +1,8 @@
-import unittest
+import os
 import pytest
 
-import wisdem.test.test_all as test_all
-import os
-import sys
+testpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + os.path.sep + \
+    'wisdem' + os.path.sep + 'test' + os.path.sep
 
-testpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + os.path.sep + 'wisdem' + os.path.sep + 'test' + os.path.sep
+sys.exit( pytest.main([testpath] )
 
-mytests = [testpath + m for m in test_all.valid_tests]
-
-if __name__ == '__main__':
-    ret = pytest.main(mytests)
-    sys.exit(ret)
-    #unittest.TextTestRunner().run(test_all.suite())
