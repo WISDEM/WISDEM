f8e4e7415a867bc6bad000245a42101231266148
Garrett Barter <garrett.barter@nrel.gov>
2020-06-20 08:12:03 -0600

    removing use of deprecated Material file

--- wisdem/commonse/__init__.py
+++ wisdem/commonse/__init__.py
@@ -1,6 +1,5 @@
 from .csystem import DirectionVector
 from .utilities import cosd, sind, tand
-from .Material import Material
 from .tube import Tube
 from .WindWaveDrag import AeroHydroLoads, CylinderWindDrag, CylinderWaveDrag
 from .akima import Akima
