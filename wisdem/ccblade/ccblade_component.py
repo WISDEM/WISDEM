0dba4a6e9a7ba474e36f7e166836713f32bb03a1
John Jasa <johnjasa11@gmail.com>
2020-07-17 16:24:25 -0600

    Updated CCBlade deriv testing. Removed test_gradients. Replaced with OpenMDAO-based deriv checks

--- wisdem/ccblade/ccblade_component.py
+++ wisdem/ccblade/ccblade_component.py
@@ -387,6 +387,8 @@ class CCBladeTwist(ExplicitComponent):
         self.add_output('DragF', val=np.zeros(n_span), units='N/m',   desc='Distributed drag force')
         self.add_output('L_n_opt', val=np.zeros(n_opt),units='N/m',   desc='Distributed lift force')
         self.add_output('D_n_opt', val=np.zeros(n_opt),units='N/m',   desc='Distributed drag force')
+        
+        self.declare_partials('*', '*', method='fd')
                 
     def compute(self, inputs, outputs, discrete_inputs, discrete_outputs):
         
@@ -411,8 +413,8 @@ class CCBladeTwist(ExplicitComponent):
             margin2stall = self.options['opt_options']['constraints']['blade']['stall']['margin'] * 180. / np.pi
             Re      = np.array(Omega * inputs['r'] * inputs['chord'] * inputs['rho'] / inputs['mu'])
             for i in range(self.n_span):
-                unsteady = eval_unsteady(inputs['airfoils_aoa'], inputs['airfoils_cl'][i,:,0,0], inputs['airfoils_cd'][i,:,0,0], inputs['airfoils_cm'][i,:,0,0])
-                alpha[i]    = (unsteady['alpha1'] - margin2stall) / 180. * np.pi
+                af[i].eval_unsteady(inputs['airfoils_aoa'], inputs['airfoils_cl'][i,:,0,0], inputs['airfoils_cd'][i,:,0,0], inputs['airfoils_cm'][i,:,0,0])
+                alpha[i]    = (af[i].unsteady['alpha1'] - margin2stall) / 180. * np.pi
                 cl[i], cd[i]    = af[i].evaluate(alpha[i], Re[i])
             Eff   = cl/cd
 
@@ -722,7 +724,7 @@ class AeroHubLoads(ExplicitComponent):
         ccblade = CCBlade(r, chord, theta, af, Rhub, Rtip, B,
             rho, mu, precone, tilt, yaw, shearExp, hub_height,
             nSector, precurve, precurveTip, tiploss=tiploss, hubloss=hubloss,
-            wakerotation=wakerotation, usecd=usecd, derivatives=True)
+            wakerotation=wakerotation, usecd=usecd, derivatives=False)
 
         azimuth_blades  = np.linspace(0, 360, B+1)
 
@@ -780,3 +782,257 @@ class AeroHubLoads(ExplicitComponent):
         outputs['Mxyz_hub_aero'] = M_hub_tot
 
 
+class CCBladeEvaluate(ExplicitComponent):
+    """
+    Standalone component for CCBlade that is only a light wrapper on CCBlade().
+    
+    Currently, this component is not used in any workflow, but it is a
+    convenient way to test the derivatives coming out of CCBlade using OpenMDAO's
+    check_partials method.
+    
+    """
+    
+    def initialize(self):
+        self.options.declare('analysis_options')
+        
+    def setup(self):
+        blade_init_options = self.options['analysis_options']['blade']
+        self.n_span        = n_span    = blade_init_options['n_span']
+        af_init_options = self.options['analysis_options']['airfoils']
+        self.n_aoa         = n_aoa     = af_init_options['n_aoa']  # Number of angle of attacks
+        self.n_Re          = n_Re      = af_init_options['n_Re']  # Number of Reynolds
+        self.n_tab         = n_tab     = af_init_options['n_tab']  # Number of tabulated data. For distributed aerodynamic control this could be > 1
+
+        # inputs
+        self.add_input('V_load',        val=20.0, units='m/s')
+        self.add_input('Omega_load',    val=9.0, units='rpm')
+        self.add_input('pitch_load',    val=0.0, units='deg')
+
+        self.add_input('r',             val=np.zeros(n_span),   units='m')
+        self.add_input('chord',         val=np.zeros(n_span),   units='m')
+        self.add_input('theta',         val=np.zeros(n_span),   units='deg')
+        self.add_input('Rhub',          val=0.0,                units='m')
+        self.add_input('Rtip',          val=0.0,                units='m')
+        self.add_input('hub_height',    val=0.0,                units='m')
+        self.add_input('precone',       val=0.0,                units='deg')
+        self.add_input('tilt',          val=0.0,                units='deg')
+        self.add_input('yaw',           val=0.0,                units='deg')
+        self.add_input('precurve',      val=np.zeros(n_span),   units='m')
+        self.add_input('precurveTip',   val=0.0,                units='m')
+
+        # parameters
+        self.add_input('airfoils_cl',   val=np.zeros((n_span, n_aoa, n_Re, n_tab)))
+        self.add_input('airfoils_cd',   val=np.zeros((n_span, n_aoa, n_Re, n_tab)))
+        self.add_input('airfoils_cm',   val=np.zeros((n_span, n_aoa, n_Re, n_tab)))
+        self.add_input('airfoils_aoa',  val=np.zeros((n_aoa)), units='deg')
+        self.add_input('airfoils_Re',   val=np.zeros((n_Re)))
+
+        self.add_discrete_input('nBlades',  val=0)
+        self.add_input('rho',               val=0.0, units='kg/m**3')
+        self.add_input('mu',                val=0.0, units='kg/(m*s)')
+        self.add_input('shearExp',          val=0.0)
+        self.add_discrete_input('nSector',  val=4)
+        self.add_discrete_input('tiploss',  val=True)
+        self.add_discrete_input('hubloss',  val=True)
+        self.add_discrete_input('wakerotation', val=True)
+        self.add_discrete_input('usecd',    val=True)
+
+        # outputs
+        self.add_output('P',      val=0., units='W')
+        self.add_output('T',     val=0., units='N')
+        self.add_output('Q',     val=0., units='N/m')
+        
+        self.add_output('CP',      val=0.)
+        self.add_output('CT',     val=0.)
+        self.add_output('CQ',     val=0.)
+
+        self.declare_partials('*', '*')
+        self.declare_partials('*', 'airfoils*', dependent=False)
+        
+    def compute(self, inputs, outputs, discrete_inputs, discrete_outputs):
+        r = inputs['r']
+        chord = inputs['chord']
+        theta = inputs['theta']
+        Rhub = inputs['Rhub']
+        Rtip = inputs['Rtip']
+        hub_height = inputs['hub_height']
+        precone = inputs['precone']
+        tilt = inputs['tilt']
+        yaw = inputs['yaw']
+        precurve = inputs['precurve']
+        precurveTip = inputs['precurveTip']
+        B = discrete_inputs['nBlades']
+        rho = inputs['rho']
+        mu = inputs['mu']
+        shearExp = inputs['shearExp']
+        nSector = discrete_inputs['nSector']
+        tiploss = discrete_inputs['tiploss']
+        hubloss = discrete_inputs['hubloss']
+        wakerotation = discrete_inputs['wakerotation']
+        usecd = discrete_inputs['usecd']
+        V_load = inputs['V_load']
+        Omega_load = inputs['Omega_load']
+        pitch_load = inputs['pitch_load']
+        
+        if len(precurve) == 0:
+            precurve = np.zeros_like(r)
+
+        # airfoil files
+        af = [None]*self.n_span
+        for i in range(self.n_span):
+            af[i] = CCAirfoil(inputs['airfoils_aoa'], inputs['airfoils_Re'], inputs['airfoils_cl'][i,:,:,0], inputs['airfoils_cd'][i,:,:,0], inputs['airfoils_cm'][i,:,:,0])
+
+        ccblade = CCBlade(r, chord, theta, af, Rhub, Rtip, B,
+            rho, mu, precone, tilt, yaw, shearExp, hub_height,
+            nSector, precurve, precurveTip, tiploss=tiploss, hubloss=hubloss,
+            wakerotation=wakerotation, usecd=usecd, derivatives=False)
+
+        loads, _ = ccblade.evaluate(V_load, Omega_load, pitch_load)
+        outputs['P'] = loads['P']
+        outputs['T'] = loads['T']
+        outputs['Q'] = loads['Q']
+        
+        loads, _ = ccblade.evaluate(V_load, Omega_load, pitch_load, coefficients=True)
+        outputs['CP'] = loads['CP']
+        outputs['CT'] = loads['CT']
+        outputs['CQ'] = loads['CQ']
+        
+    def compute_partials(self, inputs, J, discrete_inputs):
+        r = inputs['r']
+        chord = inputs['chord']
+        theta = inputs['theta']
+        Rhub = inputs['Rhub']
+        Rtip = inputs['Rtip']
+        hub_height = inputs['hub_height']
+        precone = inputs['precone']
+        tilt = inputs['tilt']
+        yaw = inputs['yaw']
+        precurve = inputs['precurve']
+        precurveTip = inputs['precurveTip']
+        B = discrete_inputs['nBlades']
+        rho = inputs['rho']
+        mu = inputs['mu']
+        shearExp = inputs['shearExp']
+        nSector = discrete_inputs['nSector']
+        tiploss = discrete_inputs['tiploss']
+        hubloss = discrete_inputs['hubloss']
+        wakerotation = discrete_inputs['wakerotation']
+        usecd = discrete_inputs['usecd']
+        V_load = inputs['V_load']
+        Omega_load = inputs['Omega_load']
+        pitch_load = inputs['pitch_load']
+        
+        if len(precurve) == 0:
+            precurve = np.zeros_like(r)
+
+        # airfoil files
+        af = [None]*self.n_span
+        for i in range(self.n_span):
+            af[i] = CCAirfoil(inputs['airfoils_aoa'], inputs['airfoils_Re'], inputs['airfoils_cl'][i,:,:,0], inputs['airfoils_cd'][i,:,:,0], inputs['airfoils_cm'][i,:,:,0])
+
+        ccblade = CCBlade(r, chord, theta, af, Rhub, Rtip, B,
+            rho, mu, precone, tilt, yaw, shearExp, hub_height,
+            nSector, precurve, precurveTip, tiploss=tiploss, hubloss=hubloss,
+            wakerotation=wakerotation, usecd=usecd, derivatives=True)
+
+
+        loads, derivs = ccblade.evaluate(V_load, Omega_load, pitch_load)
+        
+        dP = derivs['dP']
+        J['P',     'r']             = dP['dr']
+        J['P',     'chord']         = dP['dchord']
+        J['P',     'theta']         = dP['dtheta']
+        J['P',     'Rhub']          = np.squeeze(dP['dRhub'])
+        J['P',     'Rtip']          = np.squeeze(dP['dRtip'])
+        J['P',     'hub_height']    = np.squeeze(dP['dhubHt'])
+        J['P',     'precone']       = np.squeeze(dP['dprecone'])
+        J['P',     'tilt']          = np.squeeze(dP['dtilt'])
+        J['P',     'yaw']           = np.squeeze(dP['dyaw'])
+        J['P',     'V_load']        = np.squeeze(dP['dUinf'])
+        J['P',     'Omega_load']    = np.squeeze(dP['dOmega'])
+        J['P',     'pitch_load']    = np.squeeze(dP['dpitch'])
+        J['P',     'precurve']      = dP['dprecurve']
+        J['P',     'precurveTip']      = dP['dprecurveTip']
+    
+        dT = derivs['dT']
+        J['T',     'r']             = dT['dr']
+        J['T',     'chord']         = dT['dchord']
+        J['T',     'theta']         = dT['dtheta']
+        J['T',     'Rhub']          = np.squeeze(dT['dRhub'])
+        J['T',     'Rtip']          = np.squeeze(dT['dRtip'])
+        J['T',     'hub_height']    = np.squeeze(dT['dhubHt'])
+        J['T',     'precone']       = np.squeeze(dT['dprecone'])
+        J['T',     'tilt']          = np.squeeze(dT['dtilt'])
+        J['T',     'yaw']           = np.squeeze(dT['dyaw'])
+        J['T',     'V_load']        = np.squeeze(dT['dUinf'])
+        J['T',     'Omega_load']    = np.squeeze(dT['dOmega'])
+        J['T',     'pitch_load']    = np.squeeze(dT['dpitch'])
+        J['T',     'precurve']      = dT['dprecurve']
+        J['T',     'precurveTip']      = dT['dprecurveTip']
+    
+        dQ = derivs['dQ']
+        J['Q',     'r']             = dQ['dr']
+        J['Q',     'chord']         = dQ['dchord']
+        J['Q',     'theta']         = dQ['dtheta']
+        J['Q',     'Rhub']          = np.squeeze(dQ['dRhub'])
+        J['Q',     'Rtip']          = np.squeeze(dQ['dRtip'])
+        J['Q',     'hub_height']    = np.squeeze(dQ['dhubHt'])
+        J['Q',     'precone']       = np.squeeze(dQ['dprecone'])
+        J['Q',     'tilt']          = np.squeeze(dQ['dtilt'])
+        J['Q',     'yaw']           = np.squeeze(dQ['dyaw'])
+        J['Q',     'V_load']        = np.squeeze(dQ['dUinf'])
+        J['Q',     'Omega_load']    = np.squeeze(dQ['dOmega'])
+        J['Q',     'pitch_load']    = np.squeeze(dQ['dpitch'])
+        J['Q',     'precurve']      = dQ['dprecurve']
+        J['Q',     'precurveTip']      = dQ['dprecurveTip']
+
+
+        loads, derivs = ccblade.evaluate(V_load, Omega_load, pitch_load, coefficients=True)
+        
+        dCP = derivs['dCP']
+        J['CP',     'r']             = dCP['dr']
+        J['CP',     'chord']         = dCP['dchord']
+        J['CP',     'theta']         = dCP['dtheta']
+        J['CP',     'Rhub']          = np.squeeze(dCP['dRhub'])
+        J['CP',     'Rtip']          = np.squeeze(dCP['dRtip'])
+        J['CP',     'hub_height']    = np.squeeze(dCP['dhubHt'])
+        J['CP',     'precone']       = np.squeeze(dCP['dprecone'])
+        J['CP',     'tilt']          = np.squeeze(dCP['dtilt'])
+        J['CP',     'yaw']           = np.squeeze(dCP['dyaw'])
+        J['CP',     'V_load']        = np.squeeze(dCP['dUinf'])
+        J['CP',     'Omega_load']    = np.squeeze(dCP['dOmega'])
+        J['CP',     'pitch_load']    = np.squeeze(dCP['dpitch'])
+        J['CP',     'precurve']      = dCP['dprecurve']
+        J['CP',     'precurveTip']      = dCP['dprecurveTip']
+
+        dCT = derivs['dCT']
+        J['CT',     'r']             = dCT['dr']
+        J['CT',     'chord']         = dCT['dchord']
+        J['CT',     'theta']         = dCT['dtheta']
+        J['CT',     'Rhub']          = np.squeeze(dCT['dRhub'])
+        J['CT',     'Rtip']          = np.squeeze(dCT['dRtip'])
+        J['CT',     'hub_height']    = np.squeeze(dCT['dhubHt'])
+        J['CT',     'precone']       = np.squeeze(dCT['dprecone'])
+        J['CT',     'tilt']          = np.squeeze(dCT['dtilt'])
+        J['CT',     'yaw']           = np.squeeze(dCT['dyaw'])
+        J['CT',     'V_load']        = np.squeeze(dCT['dUinf'])
+        J['CT',     'Omega_load']    = np.squeeze(dCT['dOmega'])
+        J['CT',     'pitch_load']    = np.squeeze(dCT['dpitch'])
+        J['CT',     'precurve']      = dCT['dprecurve']
+        J['CT',     'precurveTip']      = dCT['dprecurveTip']
+
+        dCQ = derivs['dCQ']
+        J['CQ',     'r']             = dCQ['dr']
+        J['CQ',     'chord']         = dCQ['dchord']
+        J['CQ',     'theta']         = dCQ['dtheta']
+        J['CQ',     'Rhub']          = np.squeeze(dCQ['dRhub'])
+        J['CQ',     'Rtip']          = np.squeeze(dCQ['dRtip'])
+        J['CQ',     'hub_height']    = np.squeeze(dCQ['dhubHt'])
+        J['CQ',     'precone']       = np.squeeze(dCQ['dprecone'])
+        J['CQ',     'tilt']          = np.squeeze(dCQ['dtilt'])
+        J['CQ',     'yaw']           = np.squeeze(dCQ['dyaw'])
+        J['CQ',     'V_load']        = np.squeeze(dCQ['dUinf'])
+        J['CQ',     'Omega_load']    = np.squeeze(dCQ['dOmega'])
+        J['CQ',     'pitch_load']    = np.squeeze(dCQ['dpitch'])
+        J['CQ',     'precurve']      = dCQ['dprecurve']
+        J['CQ',     'precurveTip']      = dCQ['dprecurveTip']
