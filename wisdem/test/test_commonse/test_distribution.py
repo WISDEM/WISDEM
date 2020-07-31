86cfd2c71898d77fa57ceb1a87c4f2699718ae34
John Jasa <johnjasa11@gmail.com>
2020-07-31 14:51:10 -0600

    Cleaning up tests' usage of IVCs

--- wisdem/test/test_commonse/test_distribution.py
+++ wisdem/test/test_commonse/test_distribution.py
@@ -17,20 +17,18 @@ class Test(unittest.TestCase):
         nspline = 10
     
         prob = om.Problem()
-    
-        ivc = prob.model.add_subsystem('ivc', om.IndepVarComp(), promotes=['*'])
-    
-        # Add some arbitrary inputs
-        ivc.add_output('x', val=np.random.rand(nspline), units='m/s')
-        ivc.add_output('xbar', val=1.5, units='m/s')
-        ivc.add_output('k', val=1.5)
-        ivc.add_output('A', val=1.2)
-    
+
         prob.model.add_subsystem('comp1', WeibullCDF(nspline=nspline), promotes_inputs=['*'])
         prob.model.add_subsystem('comp2', WeibullWithMeanCDF(nspline=nspline), promotes_inputs=['*'])
         prob.model.add_subsystem('comp3', RayleighCDF(nspline=nspline), promotes_inputs=['*'])
     
         prob.setup(force_alloc_complex=True)
+        
+        # Add some arbitrary inputs
+        prob.set_val('x', np.random.rand(nspline), units='m/s')
+        prob.set_val('xbar', 1.5, units='m/s')
+        prob.set_val('k', 1.5)
+        prob.set_val('A', 1.2)
     
         prob.run_model()
     
