615580568d824845ab30d616f0e2e20c831da8c1
John Jasa <johnjasa11@gmail.com>
2020-08-12 15:25:21 -0600

    Fixed floating promotions

--- wisdem/floatingse/floating.py
+++ wisdem/floatingse/floating.py
@@ -44,7 +44,8 @@ class FloatingSE(om.Group):
                            'hsig_wave','Tsig_wave','cd_usr','cm','loading','beta_wind','beta_wave',
                            'max_draft','max_taper','min_d_to_t','material_names',
                            'permanent_ballast_density','outfitting_factor','ballast_cost_rate',
-                           'unit_cost_mat','labor_cost_rate','painting_cost_rate','outfitting_cost_rate']
+                           'unit_cost_mat','labor_cost_rate','painting_cost_rate','outfitting_cost_rate',
+                           'wind_reference_speed', 'wind_reference_height', 'wind_z0']
         main_column_promotes = column_promotes.copy()
         main_column_promotes.append(('freeboard', 'main_freeboard'))
         
