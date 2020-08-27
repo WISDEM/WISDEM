ba06fa83df049b7f386e8d7279b6163a9a2c912e
Garrett Barter <garrett.barter@nrel.gov>
2020-08-27 13:41:10 -0600

    travis fix

--- wisdem/commonse/__init__.py
+++ wisdem/commonse/__init__.py
@@ -1,5 +1,6 @@
 from .utilities import cosd, sind, tand
 from .WindWaveDrag import AeroHydroLoads, CylinderWindDrag, CylinderWaveDrag
+from .csystem import DirectionVector
 from .akima import Akima
 from .constants import *
 NFREQ  = 6
