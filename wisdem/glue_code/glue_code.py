ad294e738a8a64dd17873cbdfd35653bc490a848
John Jasa <johnjasa11@gmail.com>
2020-11-20 19:28:50 -0700

    updated rtol for glue_code

--- wisdem/glue_code/glue_code.py
+++ wisdem/glue_code/glue_code.py
@@ -30,7 +30,8 @@ class WT_RNTA(om.Group):
             self.linear_solver = lbgs = om.LinearBlockGS()
             self.nonlinear_solver = nlbgs = om.NonlinearBlockGS()
             nlbgs.options['maxiter'] = 5
-            nlbgs.options['atol'] = nlbgs.options['atol'] = 1e-2
+            nlbgs.options['atol'] = 1e-2
+            nlbgs.options['rtol'] = 1e-8            
         
         # Analysis components
         self.add_subsystem('wt_init',   WindTurbineOntologyOpenMDAO(modeling_options = modeling_options, opt_options = opt_options), promotes=['*'])
