dd1eb3008a9f492e6e35cc2acb15a84ee1836b9b
Garrett Barter <garrett.barter@nrel.gov>
2020-11-24 14:57:49 -0700

    some forgotten files

---
+++ examples/06_drivetrain/drivetrain_geared.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+# Import needed libraries
+import openmdao.api as om
+import numpy as np
+
+from wisdem.drivetrainse.drivetrain import DrivetrainSE
+from wisdem.commonse.fileIO import save_data
+
+opt_flag = False #True
+# ---
+
+# Set input options
+opt = {}
+opt['drivetrainse'] = {}
+opt['drivetrainse']['direct'] = False
+opt['drivetrainse']['hub'] = {}
+opt['drivetrainse']['hub']['hub_gamma'] = 2.0
+opt['drivetrainse']['hub']['spinner_gamma'] = 1.5
+opt['drivetrainse']['gamma_f'] = 1.35
+opt['drivetrainse']['gamma_m'] = 1.3
+opt['drivetrainse']['gamma_n'] = 1.0
+opt['servose'] = {}
+opt['servose']['n_pc'] = 20
+opt['materials'] = {}
+opt['materials']['n_mat'] = 4
+opt['flags'] = {}
+opt['flags']['generator'] = False
+# ---
+
+# Initialize OpenMDAO problem
+prob = om.Problem()
+prob.model = DrivetrainSE(modeling_options=opt, n_dlcs=1)
+# ---
+
+# If performing optimization, set up the optimizer and problem formulation
+if opt_flag:
+    # Choose the optimizer to use
+    prob.driver = om.ScipyOptimizeDriver()
+    prob.driver.options['optimizer'] = 'SLSQP'
+    prob.driver.options['tol'] = 1e-5
+
+    # Add objective
+    prob.model.add_objective('nacelle_mass', scaler=1e-6)
+
+    # Add design variables, in this case the tower diameter and wall thicknesses
+    prob.model.add_design_var('L_12', lower=0.1, upper=5.0)
+    prob.model.add_design_var('L_h1', lower=0.1, upper=5.0)
+    prob.model.add_design_var('L_hss', lower=0.1, upper=5.0)
+    prob.model.add_design_var('hub_diameter', lower=2.0, upper=5.0)
+    prob.model.add_design_var('lss_diameter', lower=0.5, upper=6.0)
+    prob.model.add_design_var('lss_wall_thickness', lower=4e-3, upper=5e-1, ref=1e-2)
+    prob.model.add_design_var('hss_diameter', lower=0.5, upper=6.0)
+    prob.model.add_design_var('hss_wall_thickness', lower=4e-3, upper=5e-1, ref=1e-2)
+    prob.model.add_design_var('bedplate_web_thickness', lower=4e-3, upper=5e-1, ref=1e-2)
+    prob.model.add_design_var('bedplate_flange_thickness', lower=4e-3, upper=5e-1, ref=1e-2)
+    prob.model.add_design_var('bedplate_flange_width', lower=0.1, upper=2.0)
+
+    # Add constraints on the tower design
+    prob.model.add_constraint('constr_lss_vonmises', upper=1.0)
+    prob.model.add_constraint('constr_hss_vonmises', upper=1.0)
+    prob.model.add_constraint('constr_bedplate_vonmises', upper=1.0)
+    prob.model.add_constraint('constr_mb1_defl', upper=1.0)
+    prob.model.add_constraint('constr_mb2_defl', upper=1.0)
+    prob.model.add_constraint('constr_hub_diameter', lower=0.0)
+    prob.model.add_constraint('constr_length', lower=0.0)
+    prob.model.add_constraint('constr_height', lower=0.0)
+    # ---
+
+
+# Set up the OpenMDAO problem
+prob.setup()
+# ---
+
+# Set input values
+prob.set_val('machine_rating', 5.0, units='MW')
+prob['upwind']         = True
+prob['n_blades']       = 3
+prob['rotor_diameter'] = 126.0
+prob['D_top']          = 3.87
+prob['minimum_rpm']    = 6.9
+prob['rated_rpm']      = 12.1
+
+# Loading from rotor
+prob['F_hub'] = np.array([1125044.07614847, -7098.0872533, -7022.79756034]).reshape((3,1))
+prob['M_hub'] = np.array([10515165.10636333, 945938.60268626, 1042828.16100417]).reshape((3,1))
+# ---
+
+# Blade properties and hub design options
+prob['blade_mass']                  = 16403.
+prob['blades_mass']                 = 3*prob['blade_mass']
+prob['pitch_system.BRFM']           = 14239550.
+prob['pitch_system_scaling_factor'] = 0.54
+prob['blade_root_diameter']         = 3.542
+prob['flange_t2shell_t']            = 4.
+prob['flange_OD2hub_D']             = 0.5
+prob['flange_ID2flange_OD']         = 0.8
+prob['hub_in2out_circ']             = 1.2
+prob['hub_stress_concentration']    = 2.5
+prob['n_front_brackets']            = 3
+prob['n_rear_brackets']             = 3
+prob['clearance_hub_spinner']       = 1.0
+prob['spin_hole_incr']              = 1.2
+prob['spinner_gust_ws']             = 70.
+prob['hub_diameter']                = 3.0
+prob['blades_I']                    = np.r_[36494351., 17549243., 14423664., np.zeros(3)]
+# ---
+
+# Drivetrain configuration and sizing inputs
+prob['bear1.bearing_type'] = 'CARB'
+prob['bear2.bearing_type'] = 'SRB'
+prob['L_12']               = 0.368
+prob['L_h1']               = 1.912
+prob['L_hss']              = 1.5
+prob['L_generator']        = 2.0
+prob['L_gearbox']          = 1.5
+prob['overhang']           = 5.0
+prob['drive_height']       = 2.3
+prob['tilt']               = 5.0
+
+prob['planet_numbers'] = np.array([3, 3, 0])
+prob['gear_configuration'] = 'eep'
+prob['gear_ratio'] = 96.0
+
+myones = np.ones(2)
+prob['lss_diameter']              = 1.0*myones
+prob['hss_diameter']              = 0.288*myones
+prob['lss_wall_thickness']        = 0.288*myones
+prob['hss_wall_thickness']        = 0.1*myones
+prob['bedplate_web_thickness']    = 0.1
+prob['bedplate_flange_thickness'] = 0.1
+prob['bedplate_flange_width']     = 1.0
+prob['bear1.D_shaft']             = 2.2
+prob['bear2.D_shaft']             = 2.2
+# ---
+
+# Material properties
+prob['E_mat']             = np.c_[200e9*np.ones(3), 205e9*np.ones(3), 118e9*np.ones(3), [4.46E10, 1.7E10, 1.67E10]].T
+prob['G_mat']             = np.c_[79.3e9*np.ones(3), 80e9*np.ones(3), 47.6e9*np.ones(3), [3.27E9, 3.48E9, 3.5E9]].T
+prob['Xt_mat']            = np.c_[450e6*np.ones(3), 814e6*np.ones(3), 310e6*np.ones(3), [6.092E8, 3.81E7, 1.529E7]].T
+prob['rho_mat']           = np.r_[7800.0, 7850.0, 7200.0, 1940.0]
+prob['sigma_y_mat']       = np.r_[345e6, 485e6, 265e6, 18.9e6]
+prob['unit_cost_mat']     = np.r_[0.7, 0.9, 0.5, 1.9]
+prob['lss_material']      = prob['hss_material'] = 'steel_drive'
+prob['bedplate_material'] = 'steel'
+prob['hub_material']      = 'cast_iron'
+prob['spinner_material']  = 'glass_uni'
+prob['material_names']    = ['steel','steel_drive','cast_iron','glass_uni']
+# ---
+
+# Run the analysis or optimization
+if opt_flag:
+    prob.model.approx_totals()
+    prob.run_driver()
+else:
+    prob.run_model()
+save_data('drivetrain_example',prob)
+# ---
+
+# Display all inputs and outputs
+prob.model.list_inputs(units=True)
+prob.model.list_outputs(units=True)
+
+# Print out the objective, design variables and constraints
+print('nacelle_mass:', prob['nacelle_mass'])
+print('')
+print('L_h1:', prob['L_h1'])
+print('L_12:', prob['L_12'])
+print('L_lss:', prob['L_lss'])
+print('L_hss:', prob['L_hss'])
+print('L_generator:', prob['L_generator'])
+print('L_gearbox:', prob['L_gearbox'])
+print('L_bedplate:', prob['L_bedplate'])
+print('H_bedplate:', prob['H_bedplate'])
+print('hub_diameter:', prob['hub_diameter'])
+print('lss_diameter:', prob['lss_diameter'])
+print('lss_wall_thickness:', prob['lss_wall_thickness'])
+print('hss_diameter:', prob['hss_diameter'])
+print('hss_wall_thickness:', prob['hss_wall_thickness'])
+print('bedplate_web_thickness:', prob['bedplate_web_thickness'])
+print('bedplate_flange_thickness:', prob['bedplate_flange_thickness'])
+print('bedplate_flange_width:', prob['bedplate_flange_width'])
+print('')
+print('constr_lss_vonmises:', prob['constr_lss_vonmises'].flatten())
+print('constr_hss_vonmises:', prob['constr_hss_vonmises'].flatten())
+print('constr_bedplate_vonmises:', prob['constr_bedplate_vonmises'].flatten())
+print('constr_mb1_defl:', prob['constr_mb1_defl'])
+print('constr_mb2_defl:', prob['constr_mb2_defl'])
+print('constr_hub_diameter:', prob['constr_hub_diameter'])
+print('constr_length:', prob['constr_length'])
+print('constr_height:', prob['constr_height'])
+# ---
