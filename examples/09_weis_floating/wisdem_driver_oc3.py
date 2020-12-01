bda9df1f0c07747c80ef110628486bdd9654e457
pibo <ptrbortolotti@gmail.com>
2020-12-01 09:57:06 -0700

    load both yaml files

--- examples/09_weis_floating/wisdem_driver_oc3.py
+++ examples/09_weis_floating/wisdem_driver_oc3.py
@@ -4,14 +4,13 @@ import os, time, sys
 
 ## File management
 run_dir = os.path.dirname(os.path.realpath(__file__))
-# fname_wt_input          = run_dir + os.sep + 'nrel5mw-spar_oc3.yaml'
-fname_wt_input = run_dir + os.sep + "nrel5mw-semi_oc4.yaml"
+fname_wt_input_oc3 = run_dir + os.sep + "nrel5mw-spar_oc3.yaml"
 fname_modeling_options = run_dir + os.sep + "modeling_options.yaml"
 fname_analysis_options = run_dir + os.sep + "analysis_options.yaml"
 
 
 tt = time.time()
-wt_opt, modeling_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
+wt_opt, modeling_options, opt_options = run_wisdem(fname_wt_input_oc3, fname_modeling_options, fname_analysis_options)
 
 if MPI:
     rank = MPI.COMM_WORLD.Get_rank()
