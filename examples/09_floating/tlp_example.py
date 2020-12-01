57153fdcededba129308e69dc987ef3249ba54f8
pibo <ptrbortolotti@gmail.com>
2020-11-30 18:17:17 -0700

    one more floating example to fix

--- examples/09_floating/tlp_example.py
+++ examples/09_floating/tlp_example.py
@@ -46,8 +46,8 @@ opt["flags"] = {}
 opt["flags"]["monopile"] = False
 
 opt["TowerSE"] = {}
-opt["TowerSE"]["n_height"] = npts
-opt["TowerSE"]["n_layers"] = 1
+opt["TowerSE"]["n_height_tower"] = npts
+opt["TowerSE"]["n_layers_tower"] = 1
 opt["materials"] = {}
 opt["materials"]["n_mat"] = 1
 
@@ -158,7 +158,7 @@ prob["number_of_mooring_connections"] = 3  # Evenly spaced around structure
 prob["mooring_lines_per_connection"] = 1  # Evenly spaced around structure
 
 # Porperties of turbine tower
-nTower = prob.model.options["modeling_options"]["TowerSE"]["n_height"] - 1
+nTower = prob.model.options["modeling_options"]["TowerSE"]["n_height_tower"] - 1
 prob["tower_height"] = prob["hub_height"] = 77.6
 prob["tower_s"] = np.linspace(0.0, 1.0, nTower + 1)
 prob["tower_outer_diameter_in"] = np.linspace(8.0, 3.87, nTower + 1)
