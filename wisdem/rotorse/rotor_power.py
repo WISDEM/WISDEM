9ff38e15130d7f3378a9559110c278c485f8965a
Garrett Barter <garrett.barter@nrel.gov>
2020-12-21 08:21:45 -0700

    seeing if reducing tolerance speeds things up

--- wisdem/rotorse/rotor_power.py
+++ wisdem/rotorse/rotor_power.py
@@ -471,7 +471,7 @@ class ComputePowerCurve(ExplicitComponent):
                 const = {}
                 const["type"] = "eq"
                 const["fun"] = const_Urated
-                params_rated = minimize(lambda x: x[1], x0, method="slsqp", bounds=bnds, constraints=const, tol=1e-3)
+                params_rated = minimize(lambda x: x[1], x0, method="slsqp", bounds=bnds, constraints=const, tol=1e-2)
 
                 if params_rated.success and not np.isnan(params_rated.x[1]):
                     U_rated = params_rated.x[1]
@@ -497,7 +497,7 @@ class ComputePowerCurve(ExplicitComponent):
                         lambda x: np.abs(const_Urated([0.0, x])),
                         bounds=[Uhub[i - 1], Uhub[i + 1]],
                         method="bounded",
-                        options={"disp": False, "xatol": 1e-3, "maxiter": 40},
+                        options={"disp": False, "xatol": 1e-2, "maxiter": 40},
                     )["x"]
 
             Omega_rated = min([U_rated * tsr / R_tip, Omega_max])
@@ -547,8 +547,8 @@ class ComputePowerCurve(ExplicitComponent):
                             lambda x: rated_power_dist(x, Uhub[i], Omega_rpm[i]),
                             pitch0,
                             pitch0 + 10.0,
-                            xtol=1e-4,
-                            rtol=1e-5,
+                            xtol=1e-3,
+                            rtol=1e-4,
                             maxiter=40,
                             disp=False,
                         )
@@ -557,7 +557,7 @@ class ComputePowerCurve(ExplicitComponent):
                             lambda x: np.abs(rated_power_dist(x, Uhub[i], Omega_rpm[i])),
                             bounds=[pitch0 - 5.0, pitch0 + 15.0],
                             method="bounded",
-                            options={"disp": False, "xatol": 1e-3, "maxiter": 40},
+                            options={"disp": False, "xatol": 1e-2, "maxiter": 40},
                         )["x"]
 
                     myout, _ = self.ccblade.evaluate([Uhub[i]], [Omega_rpm[i]], [pitch[i]], coefficients=True)
