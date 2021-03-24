c9dfaaf68336d13e8ea818215db5c63e1ce9232e
John Jasa <johnjasa11@gmail.com>
2021-03-24 15:21:43 -0600

    Added cobyla test

--- wisdem/test/test_gluecode/test_drivers.py
+++ wisdem/test/test_gluecode/test_drivers.py
@@ -62,6 +62,21 @@ class TestRegression(unittest.TestCase):
             fname_wt_input, fname_modeling_options, new_fname_analysis_options
         )
 
+    def test_cobyla(self):
+        analysis_options = load_yaml(fname_analysis_options)
+
+        solver = "COBYLA"
+        analysis_options["driver"]["optimization"]["solver"] = solver
+        analysis_options["driver"]["optimization"]["rhobeg"] = 0.5
+        analysis_options["driver"]["optimization"]["max_iter"] = 1
+
+        new_fname_analysis_options = f"{fname_analysis_options.split('/')[-1].split('.')[0]}_{solver}.yaml"
+        write_yaml(analysis_options, new_fname_analysis_options)
+
+        wt_opt, modeling_options, opt_options = run_wisdem(
+            fname_wt_input, fname_modeling_options, new_fname_analysis_options
+        )
+
 
 def suite():
     suite = unittest.TestSuite()
