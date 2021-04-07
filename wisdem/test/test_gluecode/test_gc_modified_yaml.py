88627e9d1b59ffa562ef791406a60dcc6bdf415f
Garrett Barter <garrett.barter@nrel.gov>
2021-04-07 11:40:11 -0600

    tests passing

--- wisdem/test/test_gluecode/test_gc_modified_yaml.py
+++ wisdem/test/test_gluecode/test_gc_modified_yaml.py
@@ -30,8 +30,8 @@ class TestRegression(unittest.TestCase):
         )
 
         self.assertAlmostEqual(wt_opt["rotorse.re.precomp.blade_mass"][0], 69911.6542917299, 1)
-        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 77.84277082383369, 1)
-        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 64.1511606896, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 78.054483266, 1)
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 63.9787775662, 1)
         self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 25.3128523021, 1)
         self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 144.386, 3)
 
