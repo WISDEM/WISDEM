0afc062848d21ee01566e151358578a247480cc1
John Jasa <johnjasa11@gmail.com>
2021-08-20 15:39:21 -0600

    Plotting the jacket

--- wisdem/jacketse/geometry.py
+++ wisdem/jacketse/geometry.py
@@ -1,8 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Set up inputs
-r_foot = 20.0
-r_head = 10.0
+r_foot = 10.0
+r_head = 6.0
 n_legs = 4
 n_bays = 4  # n_x
 L = 70.0  # total height of the jacket
@@ -58,6 +59,72 @@ brace_thicknesses = t_b = tau_i * t_l[:-1]
 
 
 # Take in member properties, like diameters and thicknesses
+bay_nodes = np.zeros((n_legs, n_bays + 1, 3))
+bay_nodes[:, :, 2] = np.cumsum(lower_bay_heights)
+bay_nodes[:, :, 2] += l_osg
 
+for idx in range(n_legs):
+    tmp = np.outer(lower_bay_radii, np.array([np.cos(nu * idx), np.sin(nu * idx)]))
+    bay_nodes[idx, :, 0:2] = tmp
+
+leg_nodes = np.zeros((n_legs, n_bays + 2, 3))
+tmp = l_mi / np.cos(psi_p)
+leg_nodes[:, 1:-1, 2] = bay_nodes[:, :-1, 2] + tmp
+leg_nodes[:, -1, 2] = L
+
+leg_radii = np.interp(
+    leg_nodes[0, :, 2],
+    np.linspace(leg_nodes[0, 0, 2], leg_nodes[0, -1, 2], n_bays + 2),
+    np.linspace(r_foot, r_head, n_bays + 2),
+)
+for idx in range(n_legs):
+    tmp = np.outer(leg_radii, np.array([np.cos(nu * idx), np.sin(nu * idx)]))
+    leg_nodes[idx, :, 0:2] = tmp
+
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+
+nodes = leg_nodes
+ax.scatter(nodes[:, :, 0], nodes[:, :, 1], nodes[:, :, 2])
+
+nodes = bay_nodes
+ax.scatter(nodes[:, :, 0], nodes[:, :, 1], nodes[:, :, 2])
+
+for jdx in range(n_bays):
+    for idx in range(n_legs):
+        n1 = bay_nodes[idx, jdx]
+        n2 = bay_nodes[(idx + 1) % n_legs, (jdx + 1) % (n_bays + 1)]
+        plt.plot([n1[0], n2[0]], [n1[1], n2[1]], [n1[2], n2[2]])
+
+        n1 = bay_nodes[(idx + 1) % n_legs, jdx]
+        n2 = bay_nodes[idx, (jdx + 1) % (n_bays + 1)]
+        plt.plot([n1[0], n2[0]], [n1[1], n2[1]], [n1[2], n2[2]])
+
+for jdx in range(n_bays + 1):
+    for idx in range(n_legs):
+        n1 = leg_nodes[idx, jdx]
+        n2 = leg_nodes[idx, jdx + 1]
+        plt.plot([n1[0], n2[0]], [n1[1], n2[1]], [n1[2], n2[2]])
+
+plt.show()
+
+
+# plt.figure()
+#
+# for idx in range(n_bays+1):
+#     plt.plot(leg_nodes[0, idx:idx+2, 0], leg_nodes[0, idx:idx+2, 2])
+#     plt.plot(leg_nodes[2, idx:idx+2, 0], leg_nodes[2, idx:idx+2, 2])
+#
+# for idx in range(n_bays):
+#     plt.plot(bay_nodes[0, idx:idx+2, 0], bay_nodes[3, idx:idx+2, 2])
+#     # plt.plot(bay_nodes[1, idx:idx+2, 0], bay_nodes[0, idx:idx+2, 2])
+#
+#
+#
+#
+# plt.show()
 
 # Determine member cross-sectional properties
+
+# Eventually need to get to:
+# elements = pyframe3dd.ElementData(element, N1, N2, Az, Asx, Asy, Jz, Ixx, Iyy, E, G, roll, rho)
