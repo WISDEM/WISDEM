bead585c2440be1cf2efaeede9a637399e328484
pibo <ptrbortolotti@gmail.com>
2022-03-01 08:12:21 -0700

    fix financese tests

--- wisdem/test/test_plant_financese/test_plantfinancese.py
+++ wisdem/test/test_plant_financese/test_plantfinancese.py
@@ -24,6 +24,11 @@ class TestPlantFinance(unittest.TestCase):
 
         self.discrete_inputs["turbine_number"] = 50
 
+        self.inputs["electricity_price"] = 0.04
+        self.inputs["reserve_margin_price"] = 120
+        self.inputs["capacity_credit"] = 1.
+        self.inputs["benchmark_price"] = 0.071
+
         self.mypfin = pf.PlantFinance(verbosity=True)
 
     def testRun(self):
@@ -31,14 +36,18 @@ class TestPlantFinance(unittest.TestCase):
         self.mypfin.compute(self.inputs, self.outputs, self.discrete_inputs, {})
 
         lcoe = 1e3 * 50 * (0.12 * (1.2e3 + 7.7e3) + 7e2) / (1.6e7 * 50.0)
+        plcoe = 0.16516842105263158
         self.assertEqual(self.outputs["lcoe"], lcoe)
+        self.assertAlmostEqual(self.outputs["plcoe"], plcoe)
 
         # Wake loss way
         self.inputs["plant_aep_in"] = 0.0
         self.mypfin.compute(self.inputs, self.outputs, self.discrete_inputs, {})
 
         lcoe = 1e3 * 50 * (0.12 * (1.2e3 + 7.7e3) + 7e2) / (1.6e7 * 50.0 * (1 - 0.15))
+        plcoe = 0.1890481927710843
         self.assertEqual(self.outputs["lcoe"], lcoe)
+        self.assertAlmostEqual(self.outputs["plcoe"], plcoe)
 
     def testDerivatives(self):
         prob = Problem()
