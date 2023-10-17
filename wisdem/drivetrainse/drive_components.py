5c3dd20c9dd0a540461e83388c908337d02988aa
Garrett Barter <garrett.barter@nrel.gov>
2023-10-17 06:04:43 -0600

    now fix moment of inertia calc

--- wisdem/drivetrainse/drive_components.py
+++ wisdem/drivetrainse/drive_components.py
@@ -1105,11 +1105,14 @@ class RNA_Adder(om.ExplicitComponent):
         hub_I = util.assembleI(util.rotateI(inputs["hub_system_I"], -Cup * tilt, axis="y"))
         blades_I = util.assembleI(util.rotateI(inputs["blades_I"], -Cup * tilt, axis="y"))
         nac_I_TT = util.assembleI(inputs["nacelle_I_TT"])
-        rotor_I = blades_I + hub_I
 
         R = hub_cm
-        rotor_I_TT = rotor_I + rotor_mass * (np.dot(R, R) * np.eye(3) - np.outer(R, R))
-        outputs["rna_I_TT"] = util.unassembleI(rotor_I_TT + nac_I_TT)
+        hub_I_TT = hub_I + hub_mass * (np.dot(R, R) * np.eye(3) - np.outer(R, R))
+
+        R = blades_cm
+        blades_I_TT = blades_I + hub_mass * (np.dot(R, R) * np.eye(3) - np.outer(R, R))
+        
+        outputs["rna_I_TT"] = util.unassembleI(hub_I_TT + blades_I_TT + nac_I_TT)
 
 
 # --------------------------------------------
