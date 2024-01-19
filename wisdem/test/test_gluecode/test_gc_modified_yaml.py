234d6c91c8aeafef3ac753c00c8a31a7b3850532
pibo <ptrbortolotti@gmail.com>
2024-01-19 14:40:11 -0700

    update reg values tip deflections

--- wisdem/test/test_gluecode/test_gc_modified_yaml.py
+++ wisdem/test/test_gluecode/test_gc_modified_yaml.py
@@ -30,7 +30,7 @@ class TestRegression(unittest.TestCase):
         self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 68638.59685256994, 1) # new value: improved interpolation
         self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 78.02354127898653, 1)
         self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 86.09665065848567, 1)
-        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 25.105945564423433, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 25.39268824709836, 1)
         self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 144.386, 3)
 
 
