e616cbd93c3f5269dd998c8dbaf7d7bcdf5b4f8f
dzalkind <dzalkind@nrel.gov>
2024-03-27 13:37:19 -0600

    Add initial MHK example

---
+++ examples/09_floating/rm1_driver.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import time
+
+from wisdem.commonse.mpi_tools import MPI
+from wisdem.glue_code.runWISDEM import run_wisdem
+
+## File management
+run_dir                 = os.path.dirname( os.path.realpath(__file__) )
+wisdem_examples         = os.path.join(os.path.dirname( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) ), "WISDEM", "examples")
+fname_wt_input          = run_dir + os.sep + 'RM1.yaml'
+fname_modeling_options  = run_dir + os.sep + 'modeling_options_MHK.yaml'
+fname_analysis_options  = run_dir + os.sep + 'analysis_options.yaml'
+
+
+tt = time.time()
+wt_opt, modeling_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
+
+if MPI:
+    rank = MPI.COMM_WORLD.Get_rank()
+else:
+    rank = 0
+if rank == 0:
+    print("Run time: %f"%(time.time()-tt))
+    sys.stdout.flush()
