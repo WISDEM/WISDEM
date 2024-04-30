42e53be98b22937b56039d6649d81860e19f4f06
Garrett Barter <garrett.barter@nrel.gov>
2024-04-30 06:23:35 -0600

    fix regression tests

--- wisdem/test/test_gluecode/test_gc_yaml_floating.py
+++ wisdem/test/test_gluecode/test_gc_yaml_floating.py
@@ -30,7 +30,7 @@ class TestRegression(unittest.TestCase):
 
         self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 77.8888558791, 1)
         self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 68206.4068005262, 1) # new value: improved interpolation
-        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 94.358359845164, 1)
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 94.29792690850104, 1)
         self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 25.8310690720, 1)
 
 
