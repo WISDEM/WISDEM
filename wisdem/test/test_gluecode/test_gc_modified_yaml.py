7b4118b1d740ff6065e0ffdb46b1e80cb0905d86
Garrett Barter <garrett.barter@nrel.gov>
2024-09-14 07:13:55 -0600

    adjust truth values with updates bos

--- wisdem/test/test_gluecode/test_gc_modified_yaml.py
+++ wisdem/test/test_gluecode/test_gc_modified_yaml.py
@@ -29,7 +29,7 @@ class TestRegression(unittest.TestCase):
 
         self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 68206.4068005262, 1) # new value: improved interpolation
         self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 77.68139716114459, 1)
-        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 86.5697873533143, 1)
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 88.12460562815102, 1)
         self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 25.2532523698, 1)
         self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 144.386, 3)
 
