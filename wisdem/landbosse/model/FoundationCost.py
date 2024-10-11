0bd219917edfb232c7ef0b5b12e81fe9d7f47a54
ptrbortolotti <ptrbortolotti@gmail.com>
2024-10-11 15:28:38 -0600

    np.NAN to np.nan  in landbosse

--- wisdem/landbosse/model/FoundationCost.py
+++ wisdem/landbosse/model/FoundationCost.py
@@ -505,7 +505,7 @@ class FoundationCost(CostModule):
         # if more than one crew needed to complete within construction duration then assume that all construction happens
         # within that window and use that timeframe for weather delays; if not, use the number of days calculated
         operation_data['time_construct_bool'] = operation_data['Number of days'] > foundation_construction_time * 30
-        boolean_dictionary = {True: foundation_construction_time * 30, False: np.NAN}
+        boolean_dictionary = {True: foundation_construction_time * 30, False: np.nan}
         operation_data['time_construct_bool'] = operation_data['time_construct_bool'].map(boolean_dictionary)
         operation_data['Time construct days'] = operation_data[['time_construct_bool', 'Number of days']].min(axis=1)
         num_days = operation_data['Time construct days'].max()
