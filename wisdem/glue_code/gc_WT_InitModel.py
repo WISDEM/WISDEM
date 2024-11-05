60edb8a7abb5b521ceaed152ba6e03a8c80c5049
dzalkind <dzalkind@nrel.gov>
2024-11-05 13:15:19 -0700

    Improve error messaging for user defined member coefficients

--- wisdem/glue_code/gc_WT_InitModel.py
+++ wisdem/glue_code/gc_WT_InitModel.py
@@ -1098,7 +1098,7 @@ def assign_floating_values(wt_opt, modeling_options, floating, opt_options):
             usr_defined_flag = np.all(np.array(floating["members"][i][coeff])>0)
             coeff_length = len(floating["members"][i][coeff])
             if usr_defined_flag:
-                assert grid_length == coeff_length, f"Users define {coeff}, but the length is different from grid length. Please correct."
+                assert grid_length == coeff_length, f"Users define {coeff}, but the length is different from grid length ({grid_length}). Please correct."
 
         diameter_assigned = False
         for j, kgrp in enumerate(float_opt["members"]["groups"]):
