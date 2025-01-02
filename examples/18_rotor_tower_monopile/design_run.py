d9a204b438e228f1fb3791dac36ef6965213b9c2
Garrett Barter <garrett.barter@nrel.gov>
2025-01-02 12:47:48 -0700

    make IEA15 mdodels consistent with repo

--- examples/18_rotor_tower_monopile/design_run.py
+++ examples/18_rotor_tower_monopile/design_run.py
@@ -4,7 +4,7 @@ from wisdem.glue_code.runWISDEM import run_wisdem
 
 ## File management
 run_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep
-fname_wt_input = os.path.join(run_dir, "IEA15MW_FB_scaled.yaml")
+fname_wt_input = os.path.join(run_dir, "IEA15MW_scaled.yaml")
 fname_modeling_options = os.path.join(run_dir, "modeling_options.yaml")
 fname_analysis_options = os.path.join(run_dir, "analysis_options.yaml")
 
