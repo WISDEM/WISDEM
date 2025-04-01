a7cb89f1f6f9be398f949925c5263bd5e5f44a6e
Garrett Barter <garrett.barter@nrel.gov>
2025-04-01 12:18:27 -0600

    reset truth values

--- wisdem/test/test_gluecode/test_gc_modified_yaml.py
+++ wisdem/test/test_gluecode/test_gc_modified_yaml.py
@@ -29,7 +29,7 @@ class TestRegression(unittest.TestCase):
 
         self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 68208.64259481194, 1) # new value: improved interpolation
         self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 78.17449814864315, 1)
-        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 71.1809139381956, 1)
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 76.90528582538761, 1)
         self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 25.93986529952085, 1)
         self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 144.386, 3)
 
--- wisdem/test/test_gluecode/test_gc_yaml_floating.py
+++ wisdem/test/test_gluecode/test_gc_yaml_floating.py
@@ -27,7 +27,7 @@ class TestRegression(unittest.TestCase):
 
         self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 78.00735886752878, 1)
         self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 68375.20114828358, 1) # new value: improved interpolation
-        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 80.70724370448438, 1)
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 85.55178788445798, 1)
         self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 26.179096107022136, 1)
 
 
