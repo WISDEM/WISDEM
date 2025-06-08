ca0018579a5eec75480edf89ef02a8eded4db743
Garrett Barter <garrett.barter@nrel.gov>
2025-06-08 07:32:42 -0600

    more fixes

--- examples/16_inverse_design/inverse_spar_design.py
+++ examples/16_inverse_design/inverse_spar_design.py
@@ -9,7 +9,7 @@ from wisdem.glue_code.runWISDEM import run_wisdem
 run_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep
 wisdem_examples = os.path.dirname(os.path.dirname(run_dir))
 fname_wt_input_oc3 = os.path.join(wisdem_examples, "09_floating", "nrel5mw-spar_oc3.yaml")
-fname_modeling_options = os.path.join(wisdem_examples, "09_floating", "modeling_options_noRNA.yaml")
+fname_modeling_options = os.path.join(wisdem_examples, "09_floating", "modeling_options_noRNA_spar.yaml")
 fname_analysis_options = run_dir + os.sep + "analysis_options_spar.yaml"
 
 
