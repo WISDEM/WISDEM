9f98214ad2968ccb84a574a023beea8a1568a6ec
ptrbortolotti <ptrbortolotti@gmail.com>
2025-06-10 12:33:45 -0600

    fix floating iea15 [skip ci]

--- wisdem/test/test_gluecode/test_gc_yaml_floating.py
+++ wisdem/test/test_gluecode/test_gc_yaml_floating.py
@@ -25,10 +25,10 @@ class TestRegression(unittest.TestCase):
             fname_wt_input, fname_modeling_options, fname_analysis_options
         )
 
-        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 78.00735886752878, 1)
-        self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 68375.20114828358, 1) # new value: improved interpolation
-        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 85.55178788445798, 1)
-        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 26.179096107022136, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 77.8999255816596, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 68233.0936092383, 1) # new value: improved interpolation
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 85.65687652699307, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 25.838654170633227, 1)
 
 
 if __name__ == "__main__":
