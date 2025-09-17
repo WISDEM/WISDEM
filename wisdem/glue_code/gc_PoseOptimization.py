5c604a66912d4afe9a79dbb90c70be7a64ec5c51
dzalkind <dzalkind@nrel.gov>
2025-09-17 11:20:48 -0600

    Revert "Disable NSGA2 until we figure out where it should live"
    This reverts commit c67a02b53e73eae2c7eecf8425d4359b0b948d79.

--- wisdem/glue_code/gc_PoseOptimization.py
+++ wisdem/glue_code/gc_PoseOptimization.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import openmdao.api as om
 from scipy.interpolate import PchipInterpolator
-# from nsga2_weis.driver.nsga2_om import NSGA2Driver
+from nsga2_weis.driver.nsga2_om import NSGA2Driver
 
 class PoseOptimization(object):
     def __init__(self, wt_init, modeling_options, analysis_options):
@@ -178,7 +178,6 @@ class PoseOptimization(object):
                     wt_opt.driver.options["hotstart_file"] = opt_options["hotstart_file"] # File location of a pyopt_sparse optimization history to use to hot start the optimization. Default is None.
 
             elif opt_options["solver"] == "NSGA2":
-                raise Exception('NSGA2 is not yet supported')
                 wt_opt.driver = NSGA2Driver()
                 options_keys = [
                     "max_gen",
