f015272df803645381f0f70e72e58876c161fb01
ptrbortolotti <ptrbortolotti@gmail.com>
2025-09-30 13:02:42 -0600

    _jsonschema_validate_modified only on rank = 0

--- wisdem/inputs/validation.py
+++ wisdem/inputs/validation.py
@@ -188,12 +188,13 @@ def _validate(finput, fschema, defaults=True, removal=False, restrictive=False, 
         input_dict = MPI_load_yaml(finput) if (MPI and rank_0 == False) else load_yaml(finput)
 
     # WindIO way
-    if defaults:
-        _jsonschema_validate_modified(input_dict, schema_dict, cls=DefaultValidatingDraft7Validator, registry=registry)
-    elif removal:
-        _jsonschema_validate_modified(input_dict, schema_dict, cls=RemovalValidatingDraft7Validator, registry=registry)
-    else:
-        _jsonschema_validate_modified(input_dict, schema_dict, registry=registry)
+    if rank_0 == True:
+        if defaults:
+            _jsonschema_validate_modified(input_dict, schema_dict, cls=DefaultValidatingDraft7Validator, registry=registry)
+        elif removal:
+            _jsonschema_validate_modified(input_dict, schema_dict, cls=RemovalValidatingDraft7Validator, registry=registry)
+        else:
+            _jsonschema_validate_modified(input_dict, schema_dict, registry=registry)
 
     # Old way
     #validator = DefaultValidatingDraft7Validator if defaults else json.Draft7Validator
